aeb34eea6033efee5be5798e044a3093eb58ca5e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-14 22:17:33 -0400

    Adding in template versions of the conftest and test_gatorgrader_files.

---
+++ tests/conftest.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+# set the system path to contain the previous directory
+mypath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, mypath + '/../')
+
+
+# define the viable command-line arguments when running tests
+def pytest_addoption(parser):
+    # define command-line argument for including slow tests
+    parser.addoption(
+        "--runslow", action="store_true", help="Run the slow test cases")
+    # define command-line argument for including tests that securely download files
+    parser.addoption(
+        "--rundownload", action="store_true", help="Run the downloading test cases")
