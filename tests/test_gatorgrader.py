fc9c9430ad304fa467058cb9d41564c7f4d39c3d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 17:49:37 -0400

    Improving test cases in the test_gatorgrader and then adding test cases for command-line arguments that are not verifiable.

--- tests/test_gatorgrader.py
+++ tests/test_gatorgrader.py
@@ -20,6 +20,12 @@ def verifiable_gg_args():
     return ['--directories', 'D', 'F', 'G', '--checkfiles', 'a', 'b', 'c']
 
 
+@pytest.fixture
+def not_verifiable_gg_args():
+    """Return arguments that are verifiable"""
+    return ['--directories', 'D', 'F', 'G', '--checkfiles', 'a', 'b']
+
+
 def test_default_argument_values_correct(no_gg_args):
     """The default command-line arguments are correct"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(no_gg_args)
@@ -29,8 +35,15 @@ def test_default_argument_values_correct(no_gg_args):
 
 
 def test_gatorgrader_verified(verifiable_gg_args):
-    """Run gatorgrader with basic arguments and it is verified"""
+    """Run gatorgrader with verifiable arguments and it is verified"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(verifiable_gg_args)
-    gg_args_verified = gatorgrader.verify_gatorgrader_arguments(
-        gg_arguments)
+    gg_args_verified = gatorgrader.verify_gatorgrader_arguments(gg_arguments)
     assert gg_args_verified == VERIFIED
+
+
+def test_gatorgrader_not_verified(not_verifiable_gg_args):
+    """Run gatorgrader with not verifiable arguments and it is not verified"""
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(
+        not_verifiable_gg_args)
+    gg_args_verified = gatorgrader.verify_gatorgrader_arguments(gg_arguments)
+    assert gg_args_verified == NOT_VERIFIED
