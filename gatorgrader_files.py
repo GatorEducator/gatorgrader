fca6355063bb76386795d52993f1e62db467d5dd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 12:25:54 -0400

    Updating the gatorgrader_files and test_gatorgrader_files so that the main function now contains the word "check" in it.

--- gatorgrader_files.py
+++ gatorgrader_files.py
@@ -5,7 +5,7 @@ from pathlib import Path
 FILE_SEPARATOR = "/"
 
 
-def file_in_directory(given_file, containing_directory):
+def check_file_in_directory(given_file, containing_directory):
     """ Returns true if the specified file is in the directory """
     file_for_checking = Path(containing_directory +
                              FILE_SEPARATOR + given_file)
