16502abb99f6e0fa82ba124691046932591665ee
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 11:22:10 -0400

    Improving the first test case in test_gatorgrader_files and then adding a new test case that tests for multiple files in a directory.

--- tests/test_gatorgrader_files.py
+++ tests/test_gatorgrader_files.py
@@ -4,12 +4,27 @@ import gatorgrader_files
 
 
 def test_one_file_found_in_subdirectory(tmpdir):
-    """ file_in_directory can file one file in a subdirectory """
-    p = tmpdir.mkdir("sub").join("hello.txt")
-    p.write("content")
-    print(tmpdir.basename)
-    assert p.read() == "content"
+    """ file_in_directory can find one file file in a subdirectory """
+    hello_file = tmpdir.mkdir("sub").join("hello.txt")
+    hello_file.write("content")
+    assert hello_file.read() == "content"
     assert len(tmpdir.listdir()) == 1
     was_file_found = gatorgrader_files.file_in_directory(
         "hello.txt", tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
+    assert was_file_found is True
+
+
+def test_many_files_found_in_subdirectory(tmpdir):
+    """ file_in_directory can find many files file in a subdirectory """
+    hello_file = tmpdir.mkdir("sub").join("hello.txt")
+    hello_file.write("content")
+    assert hello_file.read() == "content"
+    assert len(tmpdir.listdir()) == 1
+    was_file_found = gatorgrader_files.file_in_directory(
+        "hello.txt", tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
+    assert was_file_found is True
+    readme_file = tmpdir.join("sub").join("README.md")
+    readme_file .write("# README")
+    was_file_found = gatorgrader_files.file_in_directory(
+        "README.md", tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
     assert was_file_found is True
