0f66cd2bd331650e525a15211347269193990d4f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 14:01:40 -0400

    Updating the conftest file for courses/all/tests/ so that it uses TRIPLE_BACKUP.

--- courses/all/tests/conftest.py
+++ courses/all/tests/conftest.py
@@ -1,7 +1,16 @@
 """Configuration file for the test_gatorgrader_submit_files test suite"""
 
+import os
+import sys
+
 DIRECTORY = "directory"
 CHECKFILE = "checkfile"
+
+TRIPLE_BACKUP = "/../../../"
+
+# main GatorGrader files are TRIPLE_BACKUP from here
+mypath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(mypath + TRIPLE_BACKUP)
 
 
 def pytest_addoption(parser):
