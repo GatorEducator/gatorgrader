374e98013818b02ceeb5e95178b5d66ccfe59a7e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 12:24:56 -0400

    Adding the two parameters to the conftest.py file for the courses/all.

--- courses/all/tests/conftest.py
+++ courses/all/tests/conftest.py
@@ -1,6 +1,7 @@
 """Configuration file for the test_gatorgrader_submit_files test suite"""
 
 DIRECTORY = "directory"
+CHECKFILE = "checkfile"
 
 
 def pytest_addoption(parser):
@@ -9,12 +10,19 @@ def pytest_addoption(parser):
         "--directory",
         action="append",
         default=[],
-        help="list of directories for GatorGrader to check")
+        help="directory for GatorGrader to check")
+    parser.addoption(
+        "--checkfile",
+        action="append",
+        default=[],
+        help="file for GatorGrader to check")
 
 
 def pytest_generate_tests(metafunc):
     """ Generate the test cases from the command-line arguments """
     if DIRECTORY in metafunc.fixturenames:
-        print(metafunc.config.getoption(DIRECTORY))
         metafunc.parametrize(DIRECTORY,
                              metafunc.config.getoption(DIRECTORY))
+    if CHECKFILE in metafunc.fixturenames:
+        metafunc.parametrize(CHECKFILE,
+                             metafunc.config.getoption(CHECKFILE))
