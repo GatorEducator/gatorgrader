de05df88326583bae0706079f8e0d76487555bf2
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 19:30:26 -0400

    Adding in the files mark skipif to the test_gatorgrader_submit_files.

--- courses/all/tests/test_gatorgrader_submit_files.py
+++ courses/all/tests/test_gatorgrader_submit_files.py
@@ -1,8 +1,16 @@
 """ GatorGrader tests for files """
 
+import pytest
+
 import gatorgrader_files
 
 
+files = pytest.mark.skipif(
+    not pytest.config.getoption("--runfiles"),
+    reason="needs the --runfiles option to run")
+
+
+@files
 def test_file_exists_in_directory(checkfile, directory):
     """ Check that the file exists in the directory """
     print("file", checkfile)
