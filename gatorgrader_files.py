26d2a50c3811400a35f103f2fafa9a15c509b8ac
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 11:16:39 -0400

    Removing the debugging statements from the gatorgrader_files.

--- gatorgrader_files.py
+++ gatorgrader_files.py
@@ -7,8 +7,6 @@ FILE_SEPARATOR = "/"
 
 def file_in_directory(given_file, containing_directory):
     """ Returns true if the specified file is in the directory """
-    print("directory:", containing_directory)
-    print("file:", given_file)
     file_for_checking = Path(containing_directory +
                              FILE_SEPARATOR + given_file)
     return file_for_checking.is_file()
