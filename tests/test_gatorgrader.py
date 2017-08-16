c3d2c85596bd6cbc5a1c65457365155aede17483
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 17:44:57 -0400

    Adding the first version of the test_gatorgrader file; all tests passing.

---
+++ tests/test_gatorgrader.py
@@ -0,0 +1,36 @@
+"""Tests for the gatorgrader module"""
+
+import pytest
+
+import gatorgrader
+
+VERIFIED = True
+NOT_VERIFIED = False
+
+
+@pytest.fixture
+def no_gg_args():
+    """Return no command-line arguments"""
+    return []
+
+
+@pytest.fixture
+def verifiable_gg_args():
+    """Return arguments that are verifiable"""
+    return ['--directories', 'D', 'F', 'G', '--checkfiles', 'a', 'b', 'c']
+
+
+def test_default_argument_values_correct(no_gg_args):
+    """The default command-line arguments are correct"""
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(no_gg_args)
+    gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
+        gg_arguments)
+    assert gatorgrader_args_verified == VERIFIED
+
+
+def test_gatorgrader_verified(verifiable_gg_args):
+    """Run gatorgrader with basic arguments and it is verified"""
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(verifiable_gg_args)
+    gg_args_verified = gatorgrader.verify_gatorgrader_arguments(
+        gg_arguments)
+    assert gg_args_verified == VERIFIED
