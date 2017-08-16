6625a7ada50e23aa04a7548d5fff43329bcb9ffb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 16:23:13 -0400

    Fixing a formatting issue in the gatorgrader_files.

--- gatorgrader_files.py
+++ gatorgrader_files.py
@@ -7,6 +7,6 @@ FILE_SEPARATOR = "/"
 
 def check_file_in_directory(given_file, containing_directory):
     """ Returns true if the specified file is in the directory """
-    file_for_checking = Path(containing_directory +
-                             FILE_SEPARATOR + given_file)
+    file_for_checking = Path(containing_directory + FILE_SEPARATOR +
+                             given_file)
     return file_for_checking.is_file()
