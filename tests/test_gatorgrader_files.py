014852af51ba0ab9513d683ab1b5a764de1fec78
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 11:29:13 -0400

    Adding more test cases for multiple files to the test_gatorgrader_files.

--- tests/test_gatorgrader_files.py
+++ tests/test_gatorgrader_files.py
@@ -24,7 +24,37 @@ def test_many_files_found_in_subdirectory(tmpdir):
         "hello.txt", tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
     assert was_file_found is True
     readme_file = tmpdir.join("sub").join("README.md")
-    readme_file .write("# README")
+    readme_file.write("# README")
     was_file_found = gatorgrader_files.file_in_directory(
         "README.md", tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
     assert was_file_found is True
+
+
+def test_one_file_not_found_in_subdirectory(tmpdir):
+    """ file_in_directory cannot find one file file in a subdirectory """
+    hello_file = tmpdir.mkdir("sub").join("hello.txt")
+    hello_file.write("content")
+    assert hello_file.read() == "content"
+    assert len(tmpdir.listdir()) == 1
+    was_file_found = gatorgrader_files.file_in_directory(
+        "hello_not_there.txt",
+        tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
+    assert was_file_found is False
+
+
+def test_many_files_not_found_in_subdirectory(tmpdir):
+    """ file_in_directory cannot find many files file in a subdirectory """
+    hello_file = tmpdir.mkdir("sub").join("hello.txt")
+    hello_file.write("content")
+    assert hello_file.read() == "content"
+    assert len(tmpdir.listdir()) == 1
+    was_file_found = gatorgrader_files.file_in_directory(
+        "hello_not_there.txt",
+        tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
+    assert was_file_found is False
+    readme_file = tmpdir.join("sub").join("README.md")
+    readme_file.write("# README")
+    was_file_found = gatorgrader_files.file_in_directory(
+        "README_not_there.md",
+        tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
+    assert was_file_found is False
