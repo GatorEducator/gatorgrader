e7359755e2f9de5b3a83ae5556eb4cd957794335
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 11:04:03 -0400

    Adding the first passing test case into the test_gatorgrader_files.

---
+++ tests/test_gatorgrader_files.py
@@ -0,0 +1,15 @@
+""" test cases for the gatorgrader_files module """
+
+import gatorgrader_files
+
+
+def test_create_file(tmpdir):
+    """ file_in_directory can file one file in a subdirectory """
+    p = tmpdir.mkdir("sub").join("hello.txt")
+    p.write("content")
+    print(tmpdir.basename)
+    assert p.read() == "content"
+    assert len(tmpdir.listdir()) == 1
+    was_file_found = gatorgrader_files.file_in_directory(
+        "hello.txt", tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub")
+    assert was_file_found is True
