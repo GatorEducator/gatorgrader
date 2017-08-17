aea5b56377c92f54dc2237464d7b877051c44e2e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 23:09:05 -0400

    Commenting out the CWD code in the test_gatorgrader_submit_files; need to revisit this later.

--- courses/all/tests/test_gatorgrader_submit_files.py
+++ courses/all/tests/test_gatorgrader_submit_files.py
@@ -1,9 +1,11 @@
 """ GatorGrader tests for files """
 
+import os
 import pytest
 
 import gatorgrader_files
 
+CWD = "."
 
 files = pytest.mark.skipif(
     not pytest.config.getoption("--runfiles"),
@@ -15,5 +17,7 @@ def test_file_exists_in_directory(checkfile, directory):
     """ Check that the file exists in the directory """
     print("file", checkfile)
     print("directory", directory)
+    # if directory == CWD:
+        # directory = os.path.basename(os.getcwd()) + ".." + "/" + ".." + "/"
     was_file_found = gatorgrader_files.check_file_in_directory(checkfile, directory)
     assert was_file_found is True
