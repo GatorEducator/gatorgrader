7122c8a98652bb1b241d2042a6baa6fd3303258a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-16 23:25:40 -0400

    Adding the use of the get_gatorgrader_home function in a function in the gatorgrader_invoke.

--- gatorgrader_invoke.py
+++ gatorgrader_invoke.py
@@ -3,6 +3,8 @@
 import os
 import pytest
 
+import gatorgrader
+
 COURSES = "courses"
 ALL = "all"
 SEPARATOR = "/"
@@ -10,7 +12,8 @@ SEPARATOR = "/"
 
 def invoke_all_file_in_directory_checks(files, directories):
     """ Repeatedly run function that runs pytest """
-    os.chdir(COURSES + SEPARATOR + ALL)
+    gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
+    os.chdir(gatorgrader_home + COURSES + SEPARATOR + ALL)
     for filecheck, directory in zip(files, directories):
         invoke_file_in_directory(filecheck, directory)
 
