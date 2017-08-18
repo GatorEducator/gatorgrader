a1ad7c6f91662355aaae71e2bac0e27b9a5dc249
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-17 21:24:17 -0400

    Adding in the first version of the gatorgrader_exit file.

---
+++ gatorgrader_exit.py
@@ -0,0 +1,8 @@
+""" Determines the correct exit codes and then quits GatorGrader """
+
+def get_code(return_values):
+    """ Get the correct exit code for all of the return values """
+    if False in return_values:
+        return 1
+    else:
+        return 0
