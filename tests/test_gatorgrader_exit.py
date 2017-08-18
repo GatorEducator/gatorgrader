4b36c2c37f3cd9880834b3abca7dde24e59e2e48
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-17 21:35:45 -0400

    Adding additional test cases to the test_gatorgrader_exit file, including a parameterized test case.

--- tests/test_gatorgrader_exit.py
+++ tests/test_gatorgrader_exit.py
@@ -11,16 +11,23 @@ def test_failing_exit():
     failing_code = gatorgrader_exit.get_code(failing_exit)
     assert failing_code == 1
 
+
 def test_failing_exit_different():
     """that true input returns yes"""
     failing_exit = [True, True, False]
     failing_code = gatorgrader_exit.get_code(failing_exit)
     assert failing_code == 1
 
+
 @pytest.mark.parametrize("return_values,expected_codes", [
+    ([], 0),
     ([True], 0),
     ([False], 1),
     ([True, True], 0),
+    ([True, False], 1),
+    ([True, False, True], 1),
+    ([False, False, False], 1),
+    ([True, True, True], 0),
 ])
 def test_exit_codes_parameterized(return_values, expected_codes):
     assert gatorgrader_exit.get_code(return_values) == expected_codes
