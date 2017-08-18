1a33a262fa39add18e00c1c062d7281d76e64457
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-17 20:46:15 -0400

    Removing a lot of commented-out code in the gatorgrader_invoke file.

--- gatorgrader_invoke.py
+++ gatorgrader_invoke.py
@@ -13,9 +13,6 @@ SEPARATOR = "/"
 
 def invoke_all_file_in_directory_checks(files, directories):
     """ Repeatedly run function that runs pytest """
-    # gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
-    # os.environ["GATORGRADER_HOME"] = gatorgrader_home
-    # os.chdir(gatorgrader_home + COURSES + SEPARATOR + ALL)
     for filecheck, directory in zip(files, directories):
         invoke_file_in_directory(filecheck, directory)
 
@@ -24,17 +21,6 @@ def invoke_file_in_directory(filecheck, directory):
     """ Runs pytest to check if the file is in a directory """
     gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home(
     )
-    print("gg home", gatorgrader_home)
-    print("had to set?", had_to_set_gatorgrader_home)
     was_file_found = gatorgrader_files.check_file_in_directory(
         filecheck, gatorgrader_home + directory)
     print("found?", was_file_found)
-    # assert was_file_found is True
-
-    # print("getting read to call pytest!")
-    # gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
-    # print("grtpytest gg home", gatorgrader_home)
-    # print("grtpytest had to set", had_to_set_gatorgrader_home)
-    # pytest.main([
-    # '-s', '--runfiles', '--directory', directory, '--checkfile', filecheck
-    # ])
