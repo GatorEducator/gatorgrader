2b780b87cd877314e3f5b7e4e35aac592e608b2a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-18 12:45:53 -0400

    Adding a first draft at the regular expression for multiline comments in the gatorgrader_comments file.

--- gatorgrader_comments.py
+++ gatorgrader_comments.py
@@ -6,7 +6,7 @@ import re
 FILE_SEPARATOR = "/"
 
 SINGLELINECOMMENT_RE = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$'
-
+MULTILINECOMMENT_RE = r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
 
 def check_file_in_directory(given_file, containing_directory):
     """ Returns true if the specified file is in the directory """
@@ -18,5 +18,12 @@ def check_file_in_directory(given_file, containing_directory):
 def count_singleline_java_comment(contents):
     """ Counts the number of singleline Java comments in the code """
     pattern = re.compile(SINGLELINECOMMENT_RE, re.MULTILINE)
+    matches = pattern.findall(contents)
+    return len(matches)
+
+
+def count_multiline_java_comment(contents):
+    """ Counts the number of multiline Java comments in the code """
+    pattern = re.compile(MULTILINECOMMENT_RE, re.MULTILINE)
     matches = pattern.findall(contents)
     return len(matches)
