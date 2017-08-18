a10c46e1308fc28d7828253c43bc60a3781b4548
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-17 20:37:04 -0400

    Commenting out a lot of code in gatorgrader_invoke that used pytest; trying a new approach that calls functions directly.

--- gatorgrader_invoke.py
+++ gatorgrader_invoke.py
@@ -4,6 +4,7 @@ import os
 import pytest
 
 import gatorgrader
+import gatorgrader_files
 
 COURSES = "courses"
 ALL = "all"
@@ -12,19 +13,26 @@ SEPARATOR = "/"
 
 def invoke_all_file_in_directory_checks(files, directories):
     """ Repeatedly run function that runs pytest """
-    gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
-    os.environ["GATORGRADER_HOME"] = gatorgrader_home
-    os.chdir(gatorgrader_home + COURSES + SEPARATOR + ALL)
+    # gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
+    # os.environ["GATORGRADER_HOME"] = gatorgrader_home
+    # os.chdir(gatorgrader_home + COURSES + SEPARATOR + ALL)
     for filecheck, directory in zip(files, directories):
         invoke_file_in_directory(filecheck, directory)
 
 
 def invoke_file_in_directory(filecheck, directory):
     """ Runs pytest to check if the file is in a directory """
-    print("getting read to call pytest!")
-    gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
-    print("grtpytest gg home", gatorgrader_home)
-    print("grtpytest had to set", had_to_set_gatorgrader_home)
-    pytest.main([
-        '-s', '--runfiles', '--directory', directory, '--checkfile', filecheck
-    ])
+    gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home(
+    )
+    was_file_found = gatorgrader_files.check_file_in_directory(
+        filecheck, gatorgrader_home + directory)
+    print("found?", was_file_found)
+    # assert was_file_found is True
+
+    # print("getting read to call pytest!")
+    # gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home()
+    # print("grtpytest gg home", gatorgrader_home)
+    # print("grtpytest had to set", had_to_set_gatorgrader_home)
+    # pytest.main([
+    # '-s', '--runfiles', '--directory', directory, '--checkfile', filecheck
+    # ])
