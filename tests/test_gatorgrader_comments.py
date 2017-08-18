797447049f6f4ad7d0b3d496dfa76f645bcadfc5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-18 12:19:31 -0400

    Adding many new parameters to the parameterized test case in the test_gatorgrader_comments.

--- tests/test_gatorgrader_comments.py
+++ tests/test_gatorgrader_comments.py
@@ -7,7 +7,21 @@ import gatorgrader_comments
 
 @pytest.mark.parametrize("code_string,expected_count", [
     ('//// hello world', 1),
+    ('//// hello // world', 1),
+    ('//// hello //// world', 1),
+    ('// hello world', 1),
+    ('// hello world && --', 1),
+    ('// hello world  __ --', 1),
+    (r'System.out.println("Hello, World!\n"); // prints hello world', 1),
+    (r'String url = "http://www.example.com"', 0),
+    (r'//\\', 1),
+    ('// "some comment"', 1),
+    (r'new URI("http://www.google.com")', 0),
+    (r'System.out.println("Escaped quote\""); // Comment', 1),
+    ('hello world', 0),
+    ('', 0),
+    ("", 0),
 ])
-def test_exit_codes_parameterized(code_string, expected_count):
+def test_singleline_comments_zero_or_one(code_string, expected_count):
     assert gatorgrader_comments.count_singleline_java_comment(
         code_string) == expected_count
