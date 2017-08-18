a46a47d6b6ad177d177a492d1bff5d8419686382
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-18 11:48:20 -0400

    Improving the name of a function in the gatorgrader_invoke file.

--- gatorgrader_invoke.py
+++ gatorgrader_invoke.py
@@ -11,14 +11,14 @@ def invoke_all_file_in_directory_checks(files, directories):
     print()
     was_file_found_list = []
     for filecheck, directory in zip(files, directories):
-        was_file_found = invoke_file_in_directory(filecheck, directory)
+        was_file_found = invoke_file_in_directory_check(filecheck, directory)
         was_file_found_list.append(was_file_found)
     print()
     print("... Done checking if files are in the expected directories")
     return was_file_found_list
 
 
-def invoke_file_in_directory(filecheck, directory):
+def invoke_file_in_directory_check(filecheck, directory):
     """ Check to see if the file is in the directory """
     gatorgrader_home, had_to_set_gatorgrader_home = gatorgrader.get_gatorgrader_home(
     )
