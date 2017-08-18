d0aeadfa452909da7cffbb296e99f700c163d3ed
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-17 21:00:26 -0400

    Adding the first version of the gatorgrader_util module.

---
+++ gatorgrader_util.py
@@ -0,0 +1,9 @@
+""" Utility functions for GatorGrader """
+
+
+def get_human_answer(boolean_value):
+    """ Return a human readable response for the boolean_value """
+    if boolean_value is True:
+        return "Yes"
+    else:
+        return "No"
