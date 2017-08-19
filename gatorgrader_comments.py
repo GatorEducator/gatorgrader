8adc5fc9c966e86cd70eda15b90d4c20deb13fd0
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-18 21:17:06 -0400

    Adding in a rough version of the gatorgrader_comments file.

--- gatorgrader_comments.py
+++ gatorgrader_comments.py
@@ -9,11 +9,18 @@ SINGLELINECOMMENT_RE = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?
 MULTILINECOMMENT_RE = r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
 
 
-def check_file_in_directory(given_file, containing_directory):
-    """ Returns true if the specified file is in the directory """
+def count_entities(given_file, containing_directory, checking_function):
+    """ Counts the number of entities for the file in the directory """
     file_for_checking = Path(containing_directory + FILE_SEPARATOR +
                              given_file)
-    return file_for_checking.is_file()
+    file_contents_count = 0
+    print("count file:", file_for_checking)
+    print("count directory:", containing_directory)
+    if file_for_checking.is_file():
+        print("It is a file!")
+        file_contents = file_for_checking.read_text()
+        file_contents_count = checking_function(file_contents)
+    return file_contents_count
 
 
 def count_singleline_java_comment(contents):
