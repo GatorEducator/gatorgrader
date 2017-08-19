59d47f845000f941e043a27a00837adbcf69c158
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-19 13:46:26 -0400

    Adding a test case to test_gatorgrader to handle arguments.

--- tests/test_gatorgrader.py
+++ tests/test_gatorgrader.py
@@ -38,6 +38,12 @@ def not_verifiable_gg_args_missing_comments():
     return ['--directories', 'D', 'F', 'G', '--singlecomments', '2']
 
 
+@pytest.fixture
+def not_verifiable_gg_args_missing_paragraphs():
+    """Return arguments that are not verifiable"""
+    return ['--directories', 'D', 'F', 'G', '--paragraphs', '2']
+
+
 def test_default_argument_values_correct(no_gg_args):
     """The default command-line arguments are correct"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(no_gg_args)
@@ -72,6 +78,13 @@ def test_default_argument_values_not_correct_when_missing(not_verifiable_gg_args
 def test_default_argument_values_not_correct_when_missing_comments(not_verifiable_gg_args_missing_comments):
     """The default command-line arguments are not correct"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing_comments)
+    gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
+        gg_arguments)
+    assert gatorgrader_args_verified == NOT_VERIFIED
+
+def test_default_argument_values_not_correct_when_missing_paragraphs(not_verifiable_gg_args_missing_paragraphs):
+    """The default command-line arguments are not correct"""
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing_paragraphs)
     gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
         gg_arguments)
     assert gatorgrader_args_verified == NOT_VERIFIED
