6ad7b883b88b338c4535adc7d7dd98d2269bb487
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-18 21:26:27 -0400

    Adding the entities_greater_than_count to the gatorgrader_comments.

--- gatorgrader_comments.py
+++ gatorgrader_comments.py
@@ -9,6 +9,16 @@ SINGLELINECOMMENT_RE = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?
 MULTILINECOMMENT_RE = r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
 
 
+def entities_greater_than_count(given_file, containing_directory,
+                                expected_count, checking_function):
+    """ Determines in the entity count is greater than expected """
+    file_entity_count = count_entities(given_file, containing_directory,
+                                       checking_function)
+    if file_entity_count >= expected_count:
+        return True
+    return False
+
+
 def count_entities(given_file, containing_directory, checking_function):
     """ Counts the number of entities for the file in the directory """
     file_for_checking = Path(containing_directory + FILE_SEPARATOR +
