842c611d1c43aef40d6af2dd56c12deb4c2ddf45
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-19 13:46:57 -0400

    Improving the formatting of the test_gatorgrader.

--- tests/test_gatorgrader.py
+++ tests/test_gatorgrader.py
@@ -67,24 +67,31 @@ def test_gatorgrader_not_verified(not_verifiable_gg_args):
     assert gg_args_verified == NOT_VERIFIED
 
 
-def test_default_argument_values_not_correct_when_missing(not_verifiable_gg_args_missing):
+def test_default_argument_values_not_correct_when_missing(
+        not_verifiable_gg_args_missing):
     """The default command-line arguments are not correct"""
-    gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing)
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(
+        not_verifiable_gg_args_missing)
     gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
         gg_arguments)
     assert gatorgrader_args_verified == NOT_VERIFIED
 
 
-def test_default_argument_values_not_correct_when_missing_comments(not_verifiable_gg_args_missing_comments):
+def test_default_argument_values_not_correct_when_missing_comments(
+        not_verifiable_gg_args_missing_comments):
     """The default command-line arguments are not correct"""
-    gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing_comments)
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(
+        not_verifiable_gg_args_missing_comments)
     gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
         gg_arguments)
     assert gatorgrader_args_verified == NOT_VERIFIED
 
-def test_default_argument_values_not_correct_when_missing_paragraphs(not_verifiable_gg_args_missing_paragraphs):
+
+def test_default_argument_values_not_correct_when_missing_paragraphs(
+        not_verifiable_gg_args_missing_paragraphs):
     """The default command-line arguments are not correct"""
-    gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing_paragraphs)
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(
+        not_verifiable_gg_args_missing_paragraphs)
     gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
         gg_arguments)
     assert gatorgrader_args_verified == NOT_VERIFIED
