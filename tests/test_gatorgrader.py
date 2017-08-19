f99c82153437436adc0e7c5e800d0175dfcd9c2b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-19 12:30:07 -0400

    Adding another test case to test_gatorgrader for multicomments. Fixing name of a test case as well.

--- tests/test_gatorgrader.py
+++ tests/test_gatorgrader.py
@@ -32,6 +32,12 @@ def not_verifiable_gg_args_missing():
     return ['--directories', 'D', 'F', 'G']
 
 
+@pytest.fixture
+def not_verifiable_gg_args_missing_comments():
+    """Return arguments that are not verifiable"""
+    return ['--directories', 'D', 'F', 'G', '--singlecomments', '2']
+
+
 def test_default_argument_values_correct(no_gg_args):
     """The default command-line arguments are correct"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(no_gg_args)
@@ -55,9 +61,17 @@ def test_gatorgrader_not_verified(not_verifiable_gg_args):
     assert gg_args_verified == NOT_VERIFIED
 
 
-def test_default_argument_values_correct_when_missing(not_verifiable_gg_args_missing):
+def test_default_argument_values_not_correct_when_missing(not_verifiable_gg_args_missing):
     """The default command-line arguments are not correct"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing)
+    gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
+        gg_arguments)
+    assert gatorgrader_args_verified == NOT_VERIFIED
+
+
+def test_default_argument_values_not_correct_when_missing_comments(not_verifiable_gg_args_missing_comments):
+    """The default command-line arguments are not correct"""
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(not_verifiable_gg_args_missing_comments)
     gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
         gg_arguments)
     assert gatorgrader_args_verified == NOT_VERIFIED
