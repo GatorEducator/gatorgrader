a8eb2d616d81a607a49af22b36a78c0482b0de27
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 12:19:43 -0400

    Adding another test cases to the test_gatorgrader for fragment command-line arguments.

--- tests/test_gatorgrader.py
+++ tests/test_gatorgrader.py
@@ -39,6 +39,12 @@ def not_verifiable_gg_args_missing_comments():
 
 
 @pytest.fixture
+def not_verifiable_gg_args_missing_fragments():
+    """Return arguments that are not verifiable"""
+    return ['--directories', 'D', 'F', 'G', '--fragmentcounts', '2', '3', '2']
+
+
+@pytest.fixture
 def not_verifiable_gg_args_missing_paragraphs():
     """Return arguments that are not verifiable"""
     return ['--directories', 'D', 'F', 'G', '--paragraphs', '2']
@@ -82,6 +88,16 @@ def test_default_argument_values_not_correct_when_missing_comments(
     """The default command-line arguments are not correct"""
     gg_arguments = gatorgrader.parse_gatorgrader_arguments(
         not_verifiable_gg_args_missing_comments)
+    gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
+        gg_arguments)
+    assert gatorgrader_args_verified == NOT_VERIFIED
+
+
+def test_default_argument_values_not_correct_when_missing_fragments(
+        not_verifiable_gg_args_missing_fragments):
+    """The default command-line arguments are not correct"""
+    gg_arguments = gatorgrader.parse_gatorgrader_arguments(
+        not_verifiable_gg_args_missing_fragments)
     gatorgrader_args_verified = gatorgrader.verify_gatorgrader_arguments(
         gg_arguments)
     assert gatorgrader_args_verified == NOT_VERIFIED
