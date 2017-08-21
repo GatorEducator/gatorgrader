ddc2a15116b635e429e3a23bb2f136fd849b2c86
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 15:31:07 -0400

    Adding the get_actual_output function to the gatorgrader_run file.

--- gatorgrader_run.py
+++ gatorgrader_run.py
@@ -3,6 +3,26 @@
 import subprocess
 
 
+TASK_MARKER = "Task :run"
+BLANK_LINE = "\n"
+
+
+def get_actual_output(output):
+    """ Returns the actual lines from the command's output """
+    actual_output = []
+    capture_output = False
+    print("output: ", output)
+    for line in output.splitlines(keepends=True):
+        print("line: ", line)
+        if BLANK_LINE == line:
+            capture_output = False
+        if capture_output is True:
+            actual_output.append(line)
+        if TASK_MARKER in line:
+            capture_output = True
+    return actual_output
+
+
 def run_command(command):
     """ Run a command and return the output and error code """
     process = subprocess.Popen(
