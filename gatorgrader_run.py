9ae5e34d388eea5183baecf228795cea7a30d472
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 16:08:46 -0400

    Adding functions to gatorgrader_run that can run various types of commands and check the size of their output.

--- gatorgrader_run.py
+++ gatorgrader_run.py
@@ -2,9 +2,23 @@
 
 import subprocess
 
-
-TASK_MARKER = "Task :run"
 BLANK_LINE = "\n"
+JAVA = "Java"
+TASK_MARKER = "Task :run"
+
+
+def specified_command_output_equals_count(command, expected_count, language):
+    """ Determines if the output is exactly equal to the count """
+    output, error = run_command(command)
+    print("output:", output)
+    if language == JAVA:
+        actual_output = get_actual_java_output(output)
+    else:
+        actual_output = get_actual_output(output)
+    actual_line_count = count_outpupt_lines(actual_output)
+    if expected_count == actual_line_count:
+        return True
+    return False
 
 
 def count_outpupt_lines(output):
@@ -27,9 +41,18 @@ def get_actual_java_output(output):
             capture_output = True
     return actual_output
 
+def get_actual_output(output):
+    """ Returns the actual lines from the command's output """
+    actual_output = []
+    print(" nj output: ", output)
+    for line in output.splitlines(keepends=False):
+        actual_output.append(line)
+    return actual_output
+
 
 def run_command(command):
     """ Run a command and return the output and error code """
+    print("Running command: ", command)
     process = subprocess.Popen(
         command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     output, error = process.communicate()
