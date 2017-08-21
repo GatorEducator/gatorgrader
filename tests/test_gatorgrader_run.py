976cf7eeccedd649368f496463fb347c21987e80
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 15:30:25 -0400

    Adding some new test cases to the test_gatorgrader_run.

--- tests/test_gatorgrader_run.py
+++ tests/test_gatorgrader_run.py
@@ -1,10 +1,17 @@
-""" test cases for the gatorgrader_exit module """
+""" test cases for the gatorgrader_run module """
 
 import gatorgrader_run
 
 
-def test_run_command_returns_ls(tmpdir):
-    """Checks that code fails"""
+def test_run_command_returns_ls():
+    """Checks that a single line is returned from command """
     output, error = gatorgrader_run.run_command("echo \"Hello!\"")
     assert error == b''
     assert output == b'Hello!\n'
+
+
+def test_capture_output_count():
+    """Checks that the count is correct"""
+    sample_output = "\n\n> Task :run\nGregory M. Kapfhammer Mon Aug 21 15:08:38 EDT 2017\nHello world.\nGradle is great.\nTravis is tremendous.\n\nBUILD SUCCESSFUL in 0s\n2 actionable tasks: 1 executed, 1 up-to-date"
+    actual_output= gatorgrader_run.get_actual_output(sample_output)
+    assert len(actual_output) == 4
