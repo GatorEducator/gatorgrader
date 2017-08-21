ee8716a9a0602fe7d0318f9e6c1d307bd57823ea
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 11:31:15 -0400

    Adding a new function and regex to he gatorgrader_comments for handling Python single-line comments.

--- gatorgrader_comments.py
+++ gatorgrader_comments.py
@@ -9,12 +9,20 @@ FILE_SEPARATOR = "/"
 # https://blog.ostermiller.org/find-comment
 
 MULTILINECOMMENT_RE = r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
-SINGLELINECOMMENT_RE = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$'
+SINGLELINECOMMENT_RE_JAVA = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$'
+SINGLELINECOMMENT_RE_PYTHON = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*#(.*)$'
 
 
 def count_singleline_java_comment(contents):
     """ Counts the number of singleline Java comments in the code """
-    pattern = re.compile(SINGLELINECOMMENT_RE, re.MULTILINE)
+    pattern = re.compile(SINGLELINECOMMENT_RE_JAVA, re.MULTILINE)
+    matches = pattern.findall(contents)
+    return len(matches)
+
+
+def count_singleline_python_comment(contents):
+    """ Counts the number of singleline Python comments in the code """
+    pattern = re.compile(SINGLELINECOMMENT_RE_PYTHON, re.MULTILINE)
     matches = pattern.findall(contents)
     return len(matches)
 
