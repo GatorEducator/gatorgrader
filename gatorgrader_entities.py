dfa78d595b654555f185ab6ea012461c728569bb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 10:43:11 -0400

    Fixing some formatting mistakes in the gatorgrader_entities file.

--- gatorgrader_entities.py
+++ gatorgrader_entities.py
@@ -5,8 +5,8 @@ from pathlib import Path
 FILE_SEPARATOR = "/"
 
 
-def entity_greater_than_count(given_file, containing_directory,
-                                expected_count, checking_function):
+def entity_greater_than_count(given_file, containing_directory, expected_count,
+                              checking_function):
     """ Determines in the entity count is greater than expected """
     file_entity_count = count_entities(given_file, containing_directory,
                                        checking_function)
@@ -24,4 +24,3 @@ def count_entities(given_file, containing_directory, checking_function):
         file_contents = file_for_checking.read_text()
         file_contents_count = checking_function(file_contents)
     return file_contents_count
-
