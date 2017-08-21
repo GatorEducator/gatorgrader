349c65505befcc2b7753782db8d35ad446ca2024
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2017-08-21 11:03:11 -0400

    Adding all of the needed fragment checking functions to the gatorgrader_fragments.

--- gatorgrader_fragments.py
+++ gatorgrader_fragments.py
@@ -1,6 +1,9 @@
 """ Utility functions that check the comments of a file """
 
+from pathlib import Path
 import re
+
+FILE_SEPARATOR = "/"
 
 # References:
 # https://stackoverflow.com/questions/18568105/how-match-a-paragraph-using-regex
@@ -22,3 +25,26 @@ def count_specified_fragment(contents, fragment):
     """ Counts the specified string fragment in the writing """
     fragment_count = contents.count(fragment)
     return fragment_count
+
+
+def specified_fragment_greater_than_count(given_file, containing_directory,
+                                          chosen_fragment, expected_count,
+                                          checking_function):
+    """ Determines if the fragment count is greater than expected for a given fragment"""
+    file_fragment_count = count_fragments(given_file, containing_directory,
+                                        chosen_fragment, checking_function)
+    if file_fragment_count >= expected_count:
+        return True
+    return False
+
+
+def count_fragments(given_file, containing_directory, chosen_fragment,
+                    checking_function):
+    """ Counts the number of fragments for the file in the directory and a fragment """
+    file_for_checking = Path(containing_directory + FILE_SEPARATOR +
+                             given_file)
+    file_contents_count = 0
+    if file_for_checking.is_file():
+        file_contents = file_for_checking.read_text()
+        file_contents_count = checking_function(file_contents, chosen_fragment)
+    return file_contents_count
