5dcde2ac953856f7f27b557e3f1d85710c64cf77
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-13 16:47:07 -0500

    Fixing the docstrings in the gatorgrader_exit.

--- gatorgrader_exit.py
+++ gatorgrader_exit.py
@@ -1,8 +1,8 @@
-""" Determines the correct exit codes and then quits GatorGrader """
+"""Determines the correct exit codes and then quits GatorGrader"""
 
 
 def get_code(return_values):
-    """ Get the correct exit code for all of the return values """
+    """Get the correct exit code for all of the return values"""
     if False in return_values:
         return 1
     return 0
