f9124101a2f421496c9faed641d443c176fd7baa
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-12 23:48:16 -0500

    Fixing the linting errors in the gatorgrader_comments file.

--- gatorgrader_comments.py
+++ gatorgrader_comments.py
@@ -8,21 +8,24 @@ FILE_SEPARATOR = "/"
 # https://stackoverflow.com/questions/15423658/regular-expression-for-single-line-comments
 # https://blog.ostermiller.org/find-comment
 
-MULTILINECOMMENT_RE = r'/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/'
-SINGLELINECOMMENT_RE_JAVA = r'^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$'
-SINGLELINECOMMENT_RE_PYTHON = r'^(?:[^"#\\]|\"(?:[^\"\\]|\\.)*\"|/(?:[^#"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*#(.*)$'
+MULTILINECOMMENT_RE = r"""/\*([^*]|[\r\n]|(\*+([^*/]|[\r\n])))*\*+/"""
+SINGLELINECOMMENT_RE_JAVA = r"""^(?:[^"/\\]|\"(?:[^\"\\]|\\.)*
+\"|/(?:[^/"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*//(.*)$"""
+SINGLELINECOMMENT_RE_PYTHON = r"""^(?:[^"#\\]|\"(?:[^\"\\]|\\.)*
+\"|/(?:[^#"\\]|\\.)|/\"(?:[^\"\\]|\\.)*\"|\\.)*#(.*)$"""
 
 
 def count_singleline_java_comment(contents):
     """ Counts the number of singleline Java comments in the code """
-    pattern = re.compile(SINGLELINECOMMENT_RE_JAVA, re.MULTILINE)
+    pattern = re.compile(SINGLELINECOMMENT_RE_JAVA, re.MULTILINE | re.VERBOSE)
     matches = pattern.findall(contents)
     return len(matches)
 
 
 def count_singleline_python_comment(contents):
     """ Counts the number of singleline Python comments in the code """
-    pattern = re.compile(SINGLELINECOMMENT_RE_PYTHON, re.MULTILINE)
+    pattern = re.compile(SINGLELINECOMMENT_RE_PYTHON,
+                         re.MULTILINE | re.VERBOSE)
     matches = pattern.findall(contents)
     return len(matches)
 
