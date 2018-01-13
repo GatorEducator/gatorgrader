889f0386a1e60e4e8a2cab8084bd4cc850bc6a8d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-13 13:28:34 -0500

    Removing configuration code not needed in the conftest file.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,3 @@ import sys
 # set the system path to contain the previous directory
 mypath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, mypath + '/../')
-
-
-def pytest_addoption(parser):
-    """define the viable command-line arguments when running tests"""
-    # define command-line argument for including slow tests
-    parser.addoption(
-        "--runslow", action="store_true", help="Run the slow test cases")
-    # define command-line argument for including tests that securely download files
-    parser.addoption(
-        "--rundownload", action="store_true", help="Run the downloading test cases")
