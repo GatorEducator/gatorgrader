20a5443dcba33b1528d213a1a48f5420ca3a6c76
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-13 16:54:01 -0500

    Fixing docstring and simplifying the code in the gatorgrader_util.

--- gatorgrader_util.py
+++ gatorgrader_util.py
@@ -2,8 +2,7 @@
 
 
 def get_human_answer(boolean_value):
-    """ Return a human readable response for the boolean_value """
+    """Return a human readable response for the boolean_value"""
     if boolean_value is True:
         return "Yes"
-    else:
-        return "No"
+    return "No"
