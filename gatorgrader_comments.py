ed6df1668fda9dfd82d227450aeada3b62ae974e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-13 16:34:17 -0500

    Fixing the docstring spacing in the gatorgrader_comments.

--- gatorgrader_comments.py
+++ gatorgrader_comments.py
@@ -16,21 +16,21 @@ SINGLELINECOMMENT_RE_PYTHON = r"""^(?:[^"#\\]|\"(?:[^\"\\]|\\.)*\"|
 
 
 def count_singleline_java_comment(contents):
-    """ Counts the number of singleline Java comments in the code """
+    """Counts the number of singleline Java comments in the code"""
     pattern = re.compile(SINGLELINECOMMENT_RE_JAVA, re.MULTILINE | re.VERBOSE)
     matches = pattern.findall(contents)
     return len(matches)
 
 
 def count_singleline_python_comment(contents):
-    """ Counts the number of singleline Python comments in the code """
+    """Counts the number of singleline Python comments in the code"""
     pattern = re.compile(SINGLELINECOMMENT_RE_PYTHON, re.MULTILINE)
     matches = pattern.findall(contents)
     return len(matches)
 
 
 def count_multiline_java_comment(contents):
-    """ Counts the number of multiline Java comments in the code """
+    """Counts the number of multiline Java comments in the code"""
     pattern = re.compile(MULTILINECOMMENT_RE, re.MULTILINE)
     matches = pattern.findall(contents)
     return len(matches)
