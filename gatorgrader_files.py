3e05b291928472f380bb5008cf38501ba261fe63
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-13 16:47:35 -0500

    Fixing the docstrings in the gatorgrader_files.

--- gatorgrader_files.py
+++ gatorgrader_files.py
@@ -1,4 +1,4 @@
-""" Utility functions that check the contents of the file system """
+"""Utility functions that check the contents of the file system"""
 
 from pathlib import Path
 
@@ -6,7 +6,7 @@ FILE_SEPARATOR = "/"
 
 
 def check_file_in_directory(given_file, containing_directory):
-    """ Returns true if the specified file is in the directory """
+    """Returns true if the specified file is in the directory"""
     file_for_checking = Path(containing_directory + FILE_SEPARATOR +
                              given_file)
     return file_for_checking.is_file()
