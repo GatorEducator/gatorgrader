a2b1559d081e9bb3f8e62d78b15b53209f7a5ec9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-01-15 17:14:59 -0500

    Adding another test case to the test_gatorgrader_repository.

--- tests/test_gatorgrader_repository.py
+++ tests/test_gatorgrader_repository.py
@@ -13,3 +13,8 @@ def test_gatorgrader_repository_not_zero_commits_extra_method():
     """Checks to ensure that GatorGrader's repository registers"""
     commits = gatorgrader_repository.get_commmits(".")
     assert gatorgrader_repository.count_commits(commits) > 1
+
+
+def test_gatorgrader_repository_not_zero_commits_greater_than():
+    """Checks to ensure that GatorGrader's repository registers"""
+    assert gatorgrader_repository.commits_greater_than_count(".", 1) is True
