e8447527340ff76bc7a457ad02ecd3bf163578e1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 16:59:57 -0400

    Updating test_gatorgrader_repository to use repository from gator.

--- tests/test_gatorgrader_repository.py
+++ tests/test_gatorgrader_repository.py
@@ -1,20 +1,20 @@
 """Test cases for the repository module"""
 
-import gatorgrader_repository
+from gator import repository
 
 
-def test_gatorgrader_repository_not_zero_commits():
+def test_repository_not_zero_commits():
     """Checks to ensure that GatorGrader's repository registers"""
-    commits = gatorgrader_repository.get_commmits(".")
+    commits = repository.get_commmits(".")
     assert len(commits) > 1
 
 
-def test_gatorgrader_repository_not_zero_commits_extra_method():
+def test_repository_not_zero_commits_extra_method():
     """Checks to ensure that GatorGrader's repository registers"""
-    commits = gatorgrader_repository.get_commmits(".")
-    assert gatorgrader_repository.count_commits(commits) > 1
+    commits = repository.get_commmits(".")
+    assert repository.count_commits(commits) > 1
 
 
-def test_gatorgrader_repository_not_zero_commits_greater_than():
+def test_repository_not_zero_commits_greater_than():
     """Checks to ensure that GatorGrader's repository registers"""
-    assert gatorgrader_repository.commits_greater_than_count(".", 1) is True
+    assert repository.commits_greater_than_count(".", 1) is True
