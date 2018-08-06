5b8264b0bf215ae4f45707ac8229e67a66dcd243
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 13:41:03 -0400

    Adding verify_gatorgrader_home and get_gatorgrader_home to gatorgrader_util.

--- gatorgrader_util.py
+++ gatorgrader_util.py
@@ -1,5 +1,34 @@
 """Utility functions"""
 
+import os
+
+SLASH = "/"
+GATORGRADER_HOME = "GATORGRADER_HOME"
+
+
+def verify_gatorgrader_home(current_gatorgrader_home):
+    """Verifies that the GATORGRADER_HOME variable is set correctly"""
+    verified_gatorgrader_home = False
+    # pylint: disable=bad-continuation
+    if (
+        current_gatorgrader_home is not None
+        and current_gatorgrader_home.endswith(SLASH) is True
+    ):
+        verified_gatorgrader_home = True
+    return verified_gatorgrader_home
+
+
+def get_gatorgrader_home():
+    """Returns the GATORGRADER_HOME"""
+    current_gatorgrader_home = os.environ.get(GATORGRADER_HOME)
+    # the current gatorgrader_home is acceptable, so use it
+    if verify_gatorgrader_home(current_gatorgrader_home) is not False:
+        gatorgrader_home = current_gatorgrader_home
+    # the current gatorgrader_home is not okay, so guess at one
+    else:
+        gatorgrader_home = os.getcwd() + SLASH
+    return gatorgrader_home
+
 
 def get_human_answer(boolean_value):
     """Return a human readable response for the boolean_value"""
