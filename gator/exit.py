a4d98341e9afebe45a43dbbeea454281d2721734
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 16:35:39 -0400

    Adding the new exit module in the gator package.

---
+++ gator/exit.py
@@ -0,0 +1,8 @@
+"""Determines the correct exit codes to help GatorGrader quit"""
+
+
+def get_code(return_values):
+    """Get the correct exit code for all of the return values"""
+    if False in return_values:
+        return 1
+    return 0
