8799fe6d73996a8c0be92d987aaa9149cb6e9c81
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 18:55:43 -0400

    Improving the test case in test_gatorgrader_display.

--- tests/test_gatorgrader_display.py
+++ tests/test_gatorgrader_display.py
@@ -9,5 +9,7 @@ def test_display_welcome_produce_output_line_count(capsys):
     """Check that the display welcome function produces output lines"""
     display.welcome_message()
     captured = capsys.readouterr()
+    counted_newlines = captured.out.count('\n')
     assert "github.com" in captured.out
+    assert counted_newlines == 4
     assert captured.err == ""
