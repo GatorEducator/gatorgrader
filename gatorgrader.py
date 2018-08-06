bd18f3959e729deb4dd68482b2b093baf51b9d25
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 12:10:52 -0400

    Reformatting the source code in the gatorgrader main file.

--- gatorgrader.py
+++ gatorgrader.py
@@ -27,33 +27,34 @@ REPOSITORY = "."
 def parse_gatorgrader_arguments(args):
     """Parses the arguments provided on the command-line"""
     gg_parser = argparse.ArgumentParser(
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
 
     gg_parser.add_argument(
-        "--nowelcome",
-        help="Do not display the welcome message",
-        action="store_true")
+        "--nowelcome", help="Do not display the welcome message", action="store_true"
+    )
 
-    gg_parser.add_argument('--checkfiles', nargs='+', type=str)
-    gg_parser.add_argument('--directories', nargs='+', type=str)
+    gg_parser.add_argument("--checkfiles", nargs="+", type=str)
+    gg_parser.add_argument("--directories", nargs="+", type=str)
 
-    gg_parser.add_argument('--singlecomments', nargs='+', type=int)
+    gg_parser.add_argument("--singlecomments", nargs="+", type=int)
 
-    gg_parser.add_argument('--multicomments', nargs='+', type=int)
+    gg_parser.add_argument("--multicomments", nargs="+", type=int)
 
-    gg_parser.add_argument('--paragraphs', nargs='+', type=int)
-    gg_parser.add_argument('--wordcount', '--sentences', nargs='+', type=int,
-                           dest='wordcount')
+    gg_parser.add_argument("--paragraphs", nargs="+", type=int)
+    gg_parser.add_argument(
+        "--wordcount", "--sentences", nargs="+", type=int, dest="wordcount"
+    )
 
-    gg_parser.add_argument('--fragments', nargs='+', type=str)
-    gg_parser.add_argument('--fragmentcounts', nargs='+', type=int)
+    gg_parser.add_argument("--fragments", nargs="+", type=str)
+    gg_parser.add_argument("--fragmentcounts", nargs="+", type=int)
 
-    gg_parser.add_argument('--languages', nargs="+", type=str)
+    gg_parser.add_argument("--languages", nargs="+", type=str)
 
-    gg_parser.add_argument('--commands', nargs='+', type=str)
-    gg_parser.add_argument('--outputlines', nargs='+', type=int)
+    gg_parser.add_argument("--commands", nargs="+", type=str)
+    gg_parser.add_argument("--outputlines", nargs="+", type=int)
 
-    gg_parser.add_argument('--commits', type=int)
+    gg_parser.add_argument("--commits", type=int)
 
     gg_arguments_finished = gg_parser.parse_args(args)
     return gg_arguments_finished
@@ -90,8 +91,11 @@ def verify_gatorgrader_arguments(args):
 def verify_gatorgrader_home(current_gatorgrader_home):
     """Verifies that the GATORGRADER_HOME variable is set correctly"""
     verified_gatorgrader_home = False
-    if (current_gatorgrader_home is not None
-            and current_gatorgrader_home.endswith(SLASH) is True):
+    # pylint: disable=bad-continuation
+    if (
+        current_gatorgrader_home is not None
+        and current_gatorgrader_home.endswith(SLASH) is True
+    ):
         verified_gatorgrader_home = True
     return verified_gatorgrader_home
 
@@ -113,9 +117,7 @@ def get_gatorgrader_home():
 def display_welcome_message():
     """Display a welcome message"""
     print()
-    print(
-        "GatorGrader: Automatically Check the Files of Programmers and Writers"
-    )
+    print("GatorGrader: Automatically Check the Files of Programmers and Writers")
     print("https://github.com/gkapfham/gatorgrader")
     print()
 
@@ -127,7 +129,7 @@ def display_checking_message():
     print()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # parse and verify the arguments
     gg_arguments = parse_gatorgrader_arguments(sys.argv[1:])
     did_verify_arguments = verify_gatorgrader_arguments(gg_arguments)
@@ -142,69 +144,77 @@ if __name__ == '__main__':
             display_checking_message()
         check_return_values = []
         # CHECK: all of the files exist in their directories
-        if (gg_arguments.directories is not None
-                and gg_arguments.checkfiles is not None):
-            current_invoke_return_values =\
-                gatorgrader_invoke.invoke_all_file_in_directory_checks(
-                    gg_arguments.checkfiles, gg_arguments.directories)
+        if gg_arguments.directories is not None and gg_arguments.checkfiles is not None:
+            current_invoke_return_values = gatorgrader_invoke.invoke_all_file_in_directory_checks(
+                gg_arguments.checkfiles, gg_arguments.directories
+            )
             check_return_values.extend(current_invoke_return_values)
             # CHECK: Java code contains 'k' single-line comments
             if gg_arguments.singlecomments is not None:
-                current_invoke_return_values =\
-                    gatorgrader_invoke.invoke_all_comment_checks(
-                        gg_arguments.checkfiles, gg_arguments.directories,
-                        gg_arguments.singlecomments, SINGLE,
-                        gg_arguments.languages)
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_comment_checks(
+                    gg_arguments.checkfiles,
+                    gg_arguments.directories,
+                    gg_arguments.singlecomments,
+                    SINGLE,
+                    gg_arguments.languages,
+                )
                 check_return_values.extend(current_invoke_return_values)
             # CHECK: Java code contains 'k' multiple-line comments
             if gg_arguments.multicomments is not None:
-                current_invoke_return_values =\
-                    gatorgrader_invoke.invoke_all_comment_checks(
-                        gg_arguments.checkfiles, gg_arguments.directories,
-                        gg_arguments.multicomments, MULTIPLE,
-                        gg_arguments.languages)
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_comment_checks(
+                    gg_arguments.checkfiles,
+                    gg_arguments.directories,
+                    gg_arguments.multicomments,
+                    MULTIPLE,
+                    gg_arguments.languages,
+                )
                 check_return_values.extend(current_invoke_return_values)
             # CHECK: Writing contains 'k' paragraphs
             if gg_arguments.paragraphs is not None:
-                current_invoke_return_values =\
-                    gatorgrader_invoke.invoke_all_paragraph_checks(
-                        gg_arguments.checkfiles, gg_arguments.directories,
-                        gg_arguments.paragraphs)
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_paragraph_checks(
+                    gg_arguments.checkfiles,
+                    gg_arguments.directories,
+                    gg_arguments.paragraphs,
+                )
                 check_return_values.extend(current_invoke_return_values)
             # CHECK: Writing all paragraphs contain 'k' words
             if gg_arguments.wordcount is not None:
-                current_invoke_return_values =\
-                    gatorgrader_invoke.invoke_all_word_count_checks(
-                        gg_arguments.checkfiles, gg_arguments.directories,
-                        gg_arguments.wordcount)
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_word_count_checks(
+                    gg_arguments.checkfiles,
+                    gg_arguments.directories,
+                    gg_arguments.wordcount,
+                )
                 check_return_values.extend(current_invoke_return_values)
             # CHECK: Content contains 'k' specified fragments
-            if (gg_arguments.fragments is not None and
-                    gg_arguments.fragmentcounts is not None):
-                current_invoke_return_values =\
-                    gatorgrader_invoke.invoke_all_fragment_checks(
-                        gg_arguments.checkfiles, gg_arguments.directories,
-                        gg_arguments.fragments, gg_arguments.fragmentcounts)
+            # pylint: disable=bad-continuation
+            if (
+                gg_arguments.fragments is not None
+                and gg_arguments.fragmentcounts is not None
+            ):
+                current_invoke_return_values = gatorgrader_invoke.invoke_all_fragment_checks(
+                    gg_arguments.checkfiles,
+                    gg_arguments.directories,
+                    gg_arguments.fragments,
+                    gg_arguments.fragmentcounts,
+                )
                 check_return_values.extend(current_invoke_return_values)
         # CHECK: Command produces 'k' lines of output
-        elif (gg_arguments.commands is not None
-              and gg_arguments.outputlines is not None):
-            current_invoke_return_values =\
-                gatorgrader_invoke.invoke_all_command_checks(
-                    gg_arguments.commands, gg_arguments.outputlines)
+        elif gg_arguments.commands is not None and gg_arguments.outputlines is not None:
+            current_invoke_return_values = gatorgrader_invoke.invoke_all_command_checks(
+                gg_arguments.commands, gg_arguments.outputlines
+            )
             check_return_values.extend(current_invoke_return_values)
         # CHECK: Command produces lines of output with the specified fragment
-        elif (gg_arguments.commands is not None
-              and gg_arguments.fragments is not None):
-            current_invoke_return_values =\
-                gatorgrader_invoke.invoke_all_command_fragment_checks(
-                    gg_arguments.commands, gg_arguments.fragments)
+        elif gg_arguments.commands is not None and gg_arguments.fragments is not None:
+            current_invoke_return_values = gatorgrader_invoke.invoke_all_command_fragment_checks(
+                gg_arguments.commands, gg_arguments.fragments
+            )
             check_return_values.extend(current_invoke_return_values)
         # CHECK: Repository contains at least 'k' commits
         elif gg_arguments.commits is not None:
-            current_invoke_return_values =\
-                gatorgrader_invoke.invoke_commits_check(REPOSITORY,
-                                                        gg_arguments.commits)
+            current_invoke_return_values = gatorgrader_invoke.invoke_commits_check(
+                REPOSITORY, gg_arguments.commits
+            )
             check_return_values.append(current_invoke_return_values)
         # The requested check is not available
         else:
