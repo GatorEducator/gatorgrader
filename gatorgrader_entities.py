1d388382f319615744ef23c627e0d1215db3edd8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 12:20:56 -0400

    Reformatting the source code in the gatorgrader_entities file.

--- gatorgrader_entities.py
+++ gatorgrader_entities.py
@@ -5,11 +5,14 @@ from pathlib import Path
 FILE_SEPARATOR = "/"
 
 
-def entity_greater_than_count(given_file, containing_directory, expected_count,
-                              checking_function):
+# pylint: disable=bad-continuation
+def entity_greater_than_count(
+    given_file, containing_directory, expected_count, checking_function
+):
     """Determines if the entity count is greater than expected"""
-    file_entity_count = count_entities(given_file, containing_directory,
-                                       checking_function)
+    file_entity_count = count_entities(
+        given_file, containing_directory, checking_function
+    )
     if file_entity_count >= expected_count:
         return True
     return False
@@ -17,8 +20,7 @@ def entity_greater_than_count(given_file, containing_directory, expected_count,
 
 def count_entities(given_file, containing_directory, checking_function):
     """Counts the number of entities for the file in the directory"""
-    file_for_checking = Path(containing_directory + FILE_SEPARATOR +
-                             given_file)
+    file_for_checking = Path(containing_directory + FILE_SEPARATOR + given_file)
     file_contents_count = 0
     if file_for_checking.is_file():
         file_contents = file_for_checking.read_text()
