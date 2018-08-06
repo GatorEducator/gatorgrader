8851b54b87fe89c387463ee6093553e2c0ae3065
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 18:41:38 -0400

    Adding the display module in the gator package.

---
+++ gator/display.py
@@ -0,0 +1,16 @@
+"""Display output for GatorGrader"""
+
+
+def welcome_message():
+    """Display a welcome message"""
+    print()
+    print("GatorGrader: Automatically Check the Files of Programmers and Writers")
+    print("https://github.com/GatorEducator/gatorgrader")
+    print()
+
+
+def checking_message():
+    """Display the checking message"""
+    print("Valid command-line arguments.")
+    print("Running the specified checks!")
+    print()
