e8c0ac42f40c3ed976f0b0f2f6938909554de3d6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-06 16:35:43 -0400

    Deleting the gatorgrader_exit file.

--- gatorgrader_exit.py
+++
@@ -1,8 +0,0 @@
-"""Determines the correct exit codes to support GatorGrader quits"""
-
-
-def get_code(return_values):
-    """Get the correct exit code for all of the return values"""
-    if False in return_values:
-        return 1
-    return 0
