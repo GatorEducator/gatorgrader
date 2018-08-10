7dd337a2ad97172d1d0d7021a3ce0f3d989868a6
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-10 11:40:57 -0400

    Add the starting implementation of the report module to track results.

---
+++ gator/report.py
@@ -0,0 +1,43 @@
+"""Stores details about a check, its status, and diagnostic information"""
+
+details = {}
+result_count = 0
+
+CHECK = "check"
+OUTCOME = "outcome"
+DIAGNOSTIC = "diagnostic"
+
+
+def create_result(check, outcome, diagnostic):
+    """Create a new result"""
+    result_dictionary = {}
+    result_dictionary[CHECK] = check
+    result_dictionary[OUTCOME] = outcome
+    result_dictionary[DIAGNOSTIC] = diagnostic
+    return result_dictionary
+
+
+def add_result(check, outcome, diagnostic):
+    """Add a new result to the details dictionary"""
+    # pylint: disable=global-statement
+    global result_count
+    new_result = create_result(check, outcome, diagnostic)
+    details[result_count] = new_result
+    new_result_count = result_count
+    result_count = result_count + 1
+    return new_result_count, new_result
+
+
+def get_details():
+    """Return the details dictionary"""
+    return details
+
+
+def get_detail(index):
+    """Return a result from the details dictionary"""
+    return details[index]
+
+
+def get_size():
+    """Return the size of the details dictionary"""
+    return len(details)
