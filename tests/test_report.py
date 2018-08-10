17e0d460db43c798aa20a5c6ac8a47373e39b02f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-10 13:25:11 -0400

    Fix the test_add_multiple_results_to_report to work correctly.

--- tests/test_report.py
+++ tests/test_report.py
@@ -56,7 +56,7 @@ def test_add_multiple_results_to_report(reset_results_dictionary):
     assert isinstance(new_result_next, dict) is True
     assert len(new_result_next) == 3
     assert report.get_size() == 2
-    assert new_result[report.CHECK] == "Check for 3 paragraphs"
-    assert new_result[report.OUTCOME] is False
-    assert new_result[report.DIAGNOSTIC] == "Only found 2 paragraphs"
+    assert new_result_next[report.CHECK] == "Check for 3 paragraphs"
+    assert new_result_next[report.OUTCOME] is False
+    assert new_result_next[report.DIAGNOSTIC] == "Only found 2 paragraphs"
     assert isinstance(report.get_details(), dict) is True
