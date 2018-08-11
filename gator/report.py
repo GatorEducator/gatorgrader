b62cac4eb46b5c46581a50f26f6880bd9d0c95ba
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-11 14:05:31 -0400

    Start implementation in report module for output of a report.

--- gator/report.py
+++ gator/report.py
@@ -1,11 +1,19 @@
 """Stores details about a check, its status, and diagnostic information"""
 
+import json
+
 details = {}
 result_count = 0
+
+FIRST = 0
 
 CHECK = "check"
 OUTCOME = "outcome"
 DIAGNOSTIC = "diagnostic"
+
+REPORT = "report"
+TEXT = "output_text"
+JSON = "output_json"
 
 
 def create_result(check, outcome, diagnostic):
@@ -53,3 +61,20 @@ def get_size():
     return len(details)
 
 
+def contains_nested_dictionary(dictionary):
+    """Determines if the provided dictionary contains another dictionary"""
+    if dictionary:
+        for item in list(dictionary.values()):
+            if isinstance(item, dict):
+                return True
+    return False
+
+
+def output(dictionary_result, dictionary_format=TEXT):
+    """Return the output that the dictionary would produce"""
+    output_function = getattr(REPORT, dictionary_format)
+    output_function(dictionary_result)
+
+
+def output_text(dictionary_result):
+    """Produce output in a textual format"""
