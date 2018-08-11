91093cb0857e84bcaf12127895f4e6ab4631382d
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-11 15:45:00 -0400

    Add tests to test_util for the JSON detection function in util module.

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,6 @@
 """Test cases for the run module"""
 
+import json
 import os
 
 from gator import util
@@ -55,3 +56,18 @@ def test_gatorgrader_home_verification_working_notverified():
     """Checks that GATORGRADER_HOME verification is working"""
     gatorgrader_home_verified = util.verify_gatorgrader_home("/home/gkapfham")
     assert gatorgrader_home_verified == NOT_VERIFIED
+
+
+def test_json_detection_found():
+    """Check if a valid JSON string is detected as JSON"""
+    dictionary = {"a": "b"}
+    json_string = json.dumps(dictionary)
+    is_valid_json = util.is_json(json_string)
+    assert is_valid_json is True
+
+
+def test_json_detection_not_found():
+    """Check if a not valid JSON string is not detected as JSON"""
+    not_dictionary = "Command\nNot\nFound\n"
+    is_valid_json = util.is_json(not_dictionary)
+    assert is_valid_json is False
