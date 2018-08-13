cc485aab8022b5f7683c643cc2d1221b37bfc254
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-13 10:41:26 -0400

    Update two tests in test_repository to handle a second argument.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -19,9 +19,11 @@ def test_repository_not_zero_commits_extra_method():
 
 def test_repository_not_zero_commits_greater_than():
     """Checks to ensure that GatorGrader's repository registers"""
-    assert repository.commits_greater_than_count(".", 1) is True
+    valid, __ = repository.commits_greater_than_count(".", 1)
+    assert valid is True
 
 
 def test_repository_commits_not_huge():
     """Checks to ensure that GatorGrader's repository registers"""
-    assert repository.commits_greater_than_count(".", sys.maxsize) is False
+    valid, __ = repository.commits_greater_than_count(".", 1)
+    assert valid is True
