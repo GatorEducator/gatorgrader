3917a1af8769b33f7658394fbfa993db1d47d31e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-13 14:54:14 -0400

    Add the test_file_exists_in_directory_check to test_invoke.

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -21,3 +21,18 @@ def test_commit_checks(reset_results_dictionary):
     invoke.invoke_commits_check(".", sys.maxsize)
     details = report.get_details()
     assert details is not None
+
+
+# pylint: disable=unused-argument
+# pylint: disable=redefined-outer-name
+def test_file_exists_in_directory_check(reset_results_dictionary, tmpdir):
+    """Checks to that invocation of file existence check works correctly"""
+    hello_file = tmpdir.mkdir("sub").join("hello.txt")
+    hello_file.write("content")
+    assert hello_file.read() == "content"
+    assert len(tmpdir.listdir()) == 1
+    directory = tmpdir.dirname + "/" + tmpdir.basename + "/" + "sub"
+    hello_file = "hello.txt"
+    invoke.invoke_file_in_directory_check(hello_file, directory)
+    details = report.get_details()
+    assert details is not None
