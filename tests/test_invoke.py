55bb16dc4bdaadd1c6279a01d01165d760a63537
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-13 15:48:58 -0400

    Add tests and fix tests in the test_invoke.

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -53,7 +53,47 @@ def test_comment_counts_check_single_java(reset_results_dictionary, tmpdir):
     details = report.get_details()
     assert details is not None
     invoke.invoke_all_comment_checks(
-        hello_file, directory, 1, "single-line", "JAVA"
+        hello_file, directory, 1, "single-line", "Java"
+    )
+    details = report.get_details()
+    assert details is not None
+
+
+# pylint: disable=unused-argument
+# pylint: disable=redefined-outer-name
+def test_comment_counts_check_single_python(reset_results_dictionary, tmpdir):
+    """Checks to that invocation of comment counting check works correctly"""
+    hello_file = tmpdir.mkdir("subdirectory").join("Hello.py")
+    hello_file.write("# hello world")
+    assert hello_file.read() == "# hello world"
+    assert len(tmpdir.listdir()) == 1
+    directory = tmpdir.dirname + "/" + tmpdir.basename + "/" + "subdirectory"
+    hello_file = "Hello.py"
+    invoke.invoke_file_in_directory_check(hello_file, directory)
+    details = report.get_details()
+    assert details is not None
+    invoke.invoke_all_comment_checks(
+        hello_file, directory, 1, "single-line", "Python"
+    )
+    details = report.get_details()
+    assert details is not None
+
+
+# pylint: disable=unused-argument
+# pylint: disable=redefined-outer-name
+def test_comment_counts_check_multiple_java(reset_results_dictionary, tmpdir):
+    """Checks to that invocation of comment counting check works correctly"""
+    hello_file = tmpdir.mkdir("subdirectory").join("Hello.java")
+    hello_file.write("/* hello world */")
+    assert hello_file.read() == "/* hello world */"
+    assert len(tmpdir.listdir()) == 1
+    directory = tmpdir.dirname + "/" + tmpdir.basename + "/" + "subdirectory"
+    hello_file = "Hello.java"
+    invoke.invoke_file_in_directory_check(hello_file, directory)
+    details = report.get_details()
+    assert details is not None
+    invoke.invoke_all_comment_checks(
+        hello_file, directory, 1, "multiple-line", "Java"
     )
     details = report.get_details()
     assert details is not None
