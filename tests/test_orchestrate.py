f56589fb2107d99708f2bb3c59a6865a42bc6eb3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-13 20:58:30 -0400

    Add a test to test_orchestrate for the multiple comment check.

--- tests/test_orchestrate.py
+++ tests/test_orchestrate.py
@@ -119,11 +119,26 @@ def test_perform_actions_display_welcome_and_ready_check_exists(
 
 # pylint: disable=redefined-outer-name
 # pylint: disable=bad-continuation
-def test_perform_actions_display_welcome_and_ready_check_comments(
+def test_perform_actions_display_welcome_and_ready_check_comments_single(
     capsys, reset_results_dictionary
 ):
     """Check the argument verification, messages, and continue"""
     chosen_arguments = ["--directory", "D", "--file", "f", "--single", "2", "--language", "Java"]
+    exit_code = orchestrate.check(chosen_arguments)
+    captured = capsys.readouterr()
+    counted_newlines = captured.out.count("\n")
+    assert "GatorGrader" in captured.out
+    assert counted_newlines == 7
+    assert exit_code == 1
+
+
+# pylint: disable=redefined-outer-name
+# pylint: disable=bad-continuation
+def test_perform_actions_display_welcome_and_ready_check_comments_mult(
+    capsys, reset_results_dictionary
+):
+    """Check the argument verification, messages, and continue"""
+    chosen_arguments = ["--directory", "D", "--file", "f", "--multiple", "2", "--language", "Java"]
     exit_code = orchestrate.check(chosen_arguments)
     captured = capsys.readouterr()
     counted_newlines = captured.out.count("\n")
