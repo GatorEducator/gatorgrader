1e5cddec7f22ee48f176240958881ebfac01f93a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-17 15:30:08 -0400

    Add a test case to test_orchestrate for the check_words feature.

--- tests/test_orchestrate.py
+++ tests/test_orchestrate.py
@@ -160,3 +160,18 @@ def test_perform_actions_display_welcome_and_ready_check_paragraphs(
     assert "GatorGrader" in captured.out
     assert counted_newlines == 7
     assert exit_code == 1
+
+
+# pylint: disable=redefined-outer-name
+# pylint: disable=bad-continuation
+def test_perform_actions_display_welcome_and_ready_check_words(
+    capsys, reset_results_dictionary
+):
+    """Check the argument verification, messages, and continue"""
+    chosen_arguments = ["--directory", "D", "--file", "f", "--words", "2"]
+    exit_code = orchestrate.check(chosen_arguments)
+    captured = capsys.readouterr()
+    counted_newlines = captured.out.count("\n")
+    assert "GatorGrader" in captured.out
+    assert counted_newlines == 7
+    assert exit_code == 1
