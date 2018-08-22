a797fb0074dff3bdd90043a34074a86b12df6102
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-22 10:47:35 -0400

    Delete all of the not-needed variables in gatorgrader script.

--- gatorgrader.py
+++ gatorgrader.py
@@ -5,19 +5,6 @@ import sys
 from gator import orchestrate
 
 
-DEFAULT_COUNT = 0
-DEFAULT_LANGUAGE = "Java"
-INCORRECT_ARGUMENTS = 2
-NONEXISTENT_CHECKING = 3
-JAVA = "Java"
-PYTHON = "Python"
-
-SINGLE = "single-line"
-MULTIPLE = "multiple-line"
-
-REPOSITORY = "."
-
-
 if __name__ == "__main__":
     # orchestrate check(s) of the specified deliverable(s)
     exit_code = orchestrate.check(sys.argv[1:])
