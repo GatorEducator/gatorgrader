61985f01f3bcf662703665c1f7e84decec28dd63
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-22 10:11:58 -0400

    Reformat the test_orchestrate file.

--- tests/test_orchestrate.py
+++ tests/test_orchestrate.py
@@ -225,14 +225,7 @@ def test_perform_actions_display_welcome_and_ready_check_count_file(
     capsys, reset_results_dictionary
 ):
     """Check the argument verification, messages, and continue"""
-    chosen_arguments = [
-        "--directory",
-        "D",
-        "--file",
-        "f",
-        "--count",
-        "2",
-    ]
+    chosen_arguments = ["--directory", "D", "--file", "f", "--count", "2"]
     exit_code = orchestrate.check(chosen_arguments)
     captured = capsys.readouterr()
     counted_newlines = captured.out.count("\n")
