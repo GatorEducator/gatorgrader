80166820233f48f99b4e36bdbf807ddd68c2591f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2018-08-22 21:53:41 -0400

    Add new tests to test_repository to handle the exact parameter.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -18,12 +18,24 @@ def test_repository_not_zero_commits_extra_method():
 
 
 def test_repository_not_zero_commits_greater_than():
-    """Checks to ensure that GatorGrader's repository registers"""
+    """Checks to ensure that commit counts work correctly"""
     valid, __ = repository.commits_greater_than_count(".", 1)
     assert valid is True
 
 
+def test_repository_not_zero_commits_greater_than_exacted():
+    """Checks to ensure that commit counts work correctly"""
+    valid, __ = repository.commits_greater_than_count(".", 1, True)
+    assert valid is False
+
+
 def test_repository_commits_not_huge():
-    """Checks to ensure that GatorGrader's repository registers"""
+    """Checks to ensure that commit counts work correctly"""
     valid, __ = repository.commits_greater_than_count(".", sys.maxsize)
+    assert valid is False
+
+
+def test_repository_commits_not_huge_exacted():
+    """Checks to ensure that commit counts work correctly"""
+    valid, __ = repository.commits_greater_than_count(".", sys.maxsize, True)
     assert valid is False
