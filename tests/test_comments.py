aa3a071a164beb9cb8939d7cea77f3f82c42bb53
Mohammad Khan <mohammadnk1@gmail.com>
2019-02-14 14:18:43 -0500

    One test add for multiline python comments

--- tests/test_comments.py
+++ tests/test_comments.py
@@ -29,6 +29,18 @@ def test_file_contains_multiline_comment_count(tmpdir):
     )
     assert comment_count == 1
 
+def test_file_contains_multiline_python_comment_count(tmpdir):
+    """Checks that the multiline python comment count works"""
+    hello_file = tmpdir.mkdir("subdirectory").join("Hello.py")
+    string = "Hello \n World"
+    hello_file.write('"""{}"""'.format(string))
+    assert hello_file.read() == '"""Hello \n World"""'
+    assert len(tmpdir.listdir()) == 1
+    comment_count = entities.count_entities(
+        hello_file.basename, hello_file.dirname, comments.count_multiline_python_comment
+    )
+    assert comment_count == 1
+
 
 def test_file_contains_singleline_comment_greater(tmpdir):
     """Checks that the file is above the check level"""
