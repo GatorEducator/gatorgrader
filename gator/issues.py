c3cb382b83a15fe486528f768b0813f0a7aa4ac2
Zachary Andrews <andrewsz@allegheny.edu>
2019-02-14 14:27:14 -0500

    Looking at issue comments

--- gator/issues.py
+++ gator/issues.py
@@ -2,18 +2,20 @@
 from github import Github
 
 # github access
-g = Github("username", "password")
+g = Github("ZachAndrews98", "ZachAndr2020@!")
 
 # gets the repo
 repo = g.get_repo("GatorEducator/gatorgrader")
 
 # prints out the total number of events that have occurred for each state
-print("Closed Issues:", repo.get_issues(state="closed").totalCount)
-print("Open Issues:", repo.get_issues(state="open").totalCount)
-print("Total Issues:", repo.get_issues(state="all").totalCount)
+# print("Closed Issues:", repo.get_issues(state="closed").totalCount)
+# print("Open Issues:", repo.get_issues(state="open").totalCount)
+# print("Total Issues:", repo.get_issues(state="all").totalCount)
 
 # prints out all events
 for issue in repo.get_issues(state="all"):
-    print("Issue #", issue.number)
-    print(issue.title + ": ")
-    print(issue.body, "\n")
+    if issue.pull_request == None:
+        comments = issue.get_comments()
+        for comment in comments:
+            print(comment.user, ":")
+            print(comment.body)
