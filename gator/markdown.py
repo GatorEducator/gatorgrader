314472f4eec3abd48a7e9539893074730588d935
Jordan Durci <durcij@aldenv153.allegheny.edu>
2019-03-07 14:05:53 -0500

    Changed the structure to include the original counter and tag_count.

--- gator/markdown.py
+++ gator/markdown.py
@@ -13,26 +13,28 @@ def count_specified_tag(contents, tag):
     """Counts the specified markdown tag in the string contents"""
     ast = commonmark.Parser().parse(contents)
     mode_looking = True
+    counter = 0
     tag_count = 0
 
     # TODO: implement counting algorithm
     for subnode, enter in ast.walker():
         if mode_looking:
             # Check to see if the current subnode is an open node of the specified tag
-            if tag_count == 1 and subnode.t == tag and enter:
+            if counter == 1 and subnode.t == tag and enter:
                 # Stop search for nodes of the specified tag, as one has been found
+                tag_count += 1
                 mode_looking = False
         else:
             # Check to see if the current subnode is a closing node for the specified tag
-            if tag_count == 2 and subnode.t == tag and not enter:
+            if counter == 2 and subnode.t == tag and not enter:
                 # Start a search for a new specified item
                 mode_looking = True
 
         if subnode.is_container():
             if enter:
-                tag_count += 1
+                counter += 1
             else:
-                tag_count -= 1
+                counter -= 1
 
     return tag_count
 
