03a7f21734245e49350efba69b9bb7a27edd421f
Saejin Mahlau-Heinert <saejinmh@gmail.com>
2019-03-08 15:56:21 -0500

    Refactor invoke.py to use new report_result method

--- gator/invoke.py
+++ gator/invoke.py
@@ -23,16 +23,14 @@ EMPTY = b""
 SUCCESS = 0
 
 
-def update_report(status, message, diagnostic):
-    """Update the report after running a check"""
-    # found at least the required number of an entity
-    # do not produce a diagnostic message
+def report_result(status, message, diagnostic):
+    """Set the report after running a check"""
+    # passed the check, so do not produce a diagnostic message
     if status:
-        report.add_result(message, status, NO_DIAGNOSTIC)
-    # did not find at least the required number of an entity
-    # produce a diagnostic message using the actual count
+        report.set_result(message, status, NO_DIAGNOSTIC)
+    # did not pass the check, so produce a diagnostic message
     else:
-        report.add_result(message, status, diagnostic)
+        report.set_result(message, status, diagnostic)
 
 
 def invoke_commits_check(student_repository, expected_count, exact=False):
@@ -47,7 +45,7 @@ def invoke_commits_check(student_repository, expected_count, exact=False):
     else:
         message = "Repository has exactly " + str(expected_count) + " commit(s)"
     diagnostic = "Found " + str(actual_count) + " commit(s) in the Git repository"
-    update_report(did_check_pass, message, diagnostic)
+    report_result(did_check_pass, message, diagnostic)
     return did_check_pass
 
 
@@ -67,7 +65,7 @@ def invoke_file_in_directory_check(filecheck, directory):
     # note that update_report is not called because
     # there will never be a diagnostic for this invoke
     diagnostic = "Did not find file"
-    report.add_result(message, was_file_found, diagnostic)
+    report_result(was_file_found, message, diagnostic)
     return was_file_found
 
 
@@ -137,7 +135,7 @@ def invoke_all_comment_checks(
             + " comment(s)"
         )
     diagnostic = "Found " + str(actual_count) + " comment(s) in the specified file"
-    update_report(met_or_exceeded_count, message, diagnostic)
+    report_result(met_or_exceeded_count, message, diagnostic)
     return met_or_exceeded_count
 
 
@@ -171,7 +169,7 @@ def invoke_all_paragraph_checks(filecheck, directory, expected_count, exact=Fals
             + "paragraph(s)"
         )
     diagnostic = "Found " + str(actual_count) + " paragraph(s) in the specified file"
-    update_report(met_or_exceeded_count, message, diagnostic)
+    report_result(met_or_exceeded_count, message, diagnostic)
     return met_or_exceeded_count
 
 
@@ -207,7 +205,7 @@ def invoke_all_word_count_checks(filecheck, directory, expected_count, exact=Fal
     diagnostic = (
         "Found " + str(actual_count) + " word(s) in a paragraph of the specified file"
     )
-    update_report(met_or_exceeded_count, message, diagnostic)
+    report_result(met_or_exceeded_count, message, diagnostic)
     return met_or_exceeded_count
 
 
@@ -282,7 +280,7 @@ def invoke_all_fragment_checks(
         + str(actual_count)
         + " fragment(s) in the output or the specified file"
     )
-    update_report(met_or_exceeded_count, message, diagnostic)
+    report_result(met_or_exceeded_count, message, diagnostic)
     return met_or_exceeded_count
 
 
@@ -309,7 +307,7 @@ def invoke_all_command_executes_checks(command):
         command_passed = True
     message = "The command '" + str(command) + "'" + " executes correctly"
     diagnostic = "The command returned the error code " + str(command_returncode)
-    update_report(command_passed, message, diagnostic)
+    report_result(command_passed, message, diagnostic)
     return command_passed
 
 
@@ -357,7 +355,7 @@ def invoke_all_count_checks(
     diagnostic = (
         "Found " + str(actual_count) + " line(s) in the output or the specified file"
     )
-    update_report(met_or_exceeded_count, message, diagnostic)
+    report_result(met_or_exceeded_count, message, diagnostic)
     return met_or_exceeded_count
 
 
