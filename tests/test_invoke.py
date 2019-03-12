59e3955ea4c412393e60e5356c9ef22914a73ded
Zachary Leonardo <leonardoz@aldenv126.allegheny.edu>
2019-03-11 20:56:38 -0400

    Flake 8 fixes for test_invoke

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -182,6 +182,7 @@ def test_file_exists_in_directory_check_fragments_exact(
     details = report.get_details()
     assert details is not None
 
+
 # pylint: disable=unused-argument
 # pylint: disable=redefined-outer-name
 def test_content_string_check_fragments(reset_results_dictionary):
@@ -362,31 +363,6 @@ def test_comment_counts_check_multiple_java_not_enough(
     )
     details = report.get_details()
     assert details is not None
-
-
-# pylint: disable=unused-argument
-# pylint: disable=redefined-outer-name
-def test_run_command_grab_output_as_string(reset_results_dictionary, tmpdir):
-    """Checks that invocation of command produces correct captured output"""
-    tmpdir.mkdir("Hello1")
-    tmpdir.mkdir("Hello2")
-    tmpdir.mkdir("Hello3")
-    assert len(tmpdir.listdir()) == 3
-    directory = tmpdir.dirname + "/" + tmpdir.basename + "/"
-    met_or_exceeded_count = invoke.invoke_all_command_fragment_checks(
-        "ls " + directory, "Hello1", 1
-    )
-    assert met_or_exceeded_count is True
-    details = report.get_details()
-    assert details is not None
-    met_or_exceeded_count = invoke.invoke_all_command_fragment_checks(
-        "ls " + directory, "HelloNotThere", 1
-    )
-    assert met_or_exceeded_count is False
-    met_or_exceeded_count = invoke.invoke_all_command_fragment_checks(
-        "ls " + directory, "HelloNotThere", 0
-    )
-    assert met_or_exceeded_count is True
 
 
 # pylint: disable=unused-argument
