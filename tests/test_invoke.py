afd227c668b4a5bdb06dc0699e95c23fa22d07d6
leonardoz15 <leonardoz@allegheny.edu>
2019-03-13 17:16:57 -0400

    Created a new method for checking exact regex

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -202,15 +202,22 @@ def test_content_string_check_fragments(reset_results_dictionary):
 # pylint: disable=unused-argument
 # pylint: disable=redefined-outer-name
 def test_content_string_check_fragments_exact(reset_results_dictionary):
-    """Checks that the checking of exact fragments and regex works correctly"""
+    """Checks that the checking of exact fragments works correctly"""
     value = "hello world 44 fine\n\nhi there nice again\n\nff! Is now $@name again\n\n"
     invoke.invoke_all_fragment_checks(
         "hello", 1, invoke.NOTHING, invoke.NOTHING, value, True
     )
     details = report.get_details()
     assert details is not None
+
+
+# pylint: disable=unused-argument
+# pylint: disable=redefined-outer-name
+def test_content_string_check_fragments_exact(reset_results_dictionary):
+    """Checks that the checking of exact regex works correctly"""
+    value = "hello world 44 fine\n\nhi there nice again\n\nff! Is now $@name again\n\n"
     invoke.invoke_all_regex_checks(
-        "hello", 1, invoke.NOTHING, invoke.NOTHING, value, True
+    "hello", 1, invoke.NOTHING, invoke.NOTHING, value, True
     )
     details = report.get_details()
     assert details is not None
