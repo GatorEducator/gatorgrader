c426a073c1983546873cf151f4231cf017f185ac
Andrew Everitt <31443695+everitt-andrew@users.noreply.github.com>
2019-03-13 17:59:59 -0400

    Updated method name on line 225

--- gator/invoke.py
+++ gator/invoke.py
@@ -222,7 +222,7 @@ def invoke_all_fragment_checks(
 ):
     """Perform the check for a fragment existence in file or contents and return the results"""
     met_or_exceeded_count = 0
-    met_or_exceeded_count, actual_count = fragments.specified_fragment_greater_than_count(
+    met_or_exceeded_count, actual_count = fragments.specified_entity_greater_than_count(
         fragment,
         fragments.count_specified_fragment,
         expected_count,
