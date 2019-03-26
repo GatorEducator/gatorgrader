962eb6af12d39a36dc841349d8234c1a98f5c8a1
Jonathan Mendez <jonathanmendez@ewire23-91.allegheny.edu>
2019-03-26 18:25:55 -0400

    Corrects spelling error in count_multiline_python_comment.

--- gator/comments.py
+++ gator/comments.py
@@ -39,6 +39,6 @@ def count_multiline_java_comment(contents):
 
 def count_multiline_python_comment(contents):
     """Counts the number of multiline Python comments in the code"""
-    pattern = re.compile(MULTILINECOMMENT_RE_PYTHON, re.MULTILNE)
+    pattern = re.compile(MULTILINECOMMENT_RE_PYTHON, re.MULTILINE)
     matches = pattern.findall(contents)
     return len(matches)
