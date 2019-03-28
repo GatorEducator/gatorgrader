53a297a997afdad7facc176d6b5f36ed7dc9a13e
Joshua Yee <yeej2@allegheny.edu>
2019-03-28 13:30:25 -0400

    Fix test cases in test_issues
    This changes the code to use a test repository instead of gatorgrader

--- tests/test_issues.py
+++ tests/test_issues.py
@@ -6,12 +6,12 @@ from gator import issues
 TOKEN = "3e20125561f10fa4df42"
 
 
-def test_issue_made():
+def test_issue_made_pass():
     """Checks to ensure that issues are correctly being checked"""
     out, num, err = issues.check_issues_made(
         TOKEN + "ac38d5bdd114df9a0ee8",
-        "GatorEducator/gatorgrader",
-        "gkapfham",
+        "GatorEducator/test-repository",
+        "yeej2",
         1,
         "all",
     )
@@ -20,16 +20,44 @@ def test_issue_made():
     assert err == 0
 
 
-def test_issue_comment():
+def test_issues_made_fail():
+    """Checks when issues_made is less than expected"""
+    out, num, err = issues.check_issues_made(
+        TOKEN + "ac38d5bdd114df9a0ee8",
+        "GatorEducator/test-repository",
+        "yeej2",
+        2,
+        "all",
+    )
+    assert out is False
+    assert num == 1
+    assert err == 0
+
+
+def test_issue_comment_pass():
     """Checks to ensure that comments on issues are correctly being checked"""
     out, num, err = issues.check_comments_made(
         TOKEN + "ac38d5bdd114df9a0ee8",
-        "GatorEducator/gatorgrader",
-        "gkapfham",
+        "GatorEducator/test-repository",
+        "yeej2",
         1,
         "all",
     )
     assert out is True
+    assert num == 1
+    assert err == 0
+
+
+def test_issue_comment_fail():
+    """Checks to ensure that comments on issues are correctly being checked"""
+    out, num, err = issues.check_comments_made(
+        TOKEN + "ac38d5bdd114df9a0ee8",
+        "GatorEducator/test-repository",
+        "yeej2",
+        2,
+        "all",
+    )
+    assert out is False
     assert num == 1
     assert err == 0
 
@@ -37,7 +65,7 @@ def test_issue_comment():
 def test_issue_invalid_token():
     """Checks to ensure that an incorrect token returns the correct error"""
     __, __, err = issues.check_issues_made(
-        "aaa", "GatorEducator/gatorgrader", "gkapfham", 1, "all"
+        "aaa", "GatorEducator/test-repository", "yeej2", 1, "all"
     )
     assert err == -1
 
@@ -46,7 +74,7 @@ def test_issue_invalid_token():
 def test_issue_invalid_repo():
     """Checks to ensure that if there is an incorrect repo it returns the correct error"""
     __, __, err = issues.check_issues_made(
-        TOKEN + "ac38d5bdd114df9a0ee8", "GatorEducator/gator", "gkapfham", 1, "all"
+        TOKEN + "ac38d5bdd114df9a0ee8", "GatorEducator/gator", "yeej2", 1, "all"
     )
     assert err == -2
 
@@ -54,7 +82,7 @@ def test_issue_invalid_repo():
 # def test_check_issues_made():
 #     """Checks the final output of check_issues_made"""
 #     actual_output = issues.check_issues_made(
-#         TOKEN + "ac38d5bdd114df9a0ee8", "GatorEducator/gatorgrader", "gkapfham", 1, "all"
+#         TOKEN + "ac38d5bdd114df9a0ee8", "GatorEducator/test-repository", "yeej2", 1, "all"
 #     )
 #     expected_output =
 
@@ -63,7 +91,7 @@ def test_issue_invalid_repo():
 def test_comments_invalid_token():
     """Checks to ensure that if there is an incorrect token it returns the correct error"""
     __, __, err = issues.check_comments_made(
-        "aaa", "GatorEducator/gatorgrader", "gkapfham", 1, "all"
+        "aaa", "GatorEducator/test-repository", "yeej2", 1, "all"
     )
     assert err == -1
 
@@ -71,6 +99,6 @@ def test_comments_invalid_token():
 def test_comments_invalid_repo():
     """Checks to ensure that if there is an incorrect repo it returns the correct error"""
     __, __, err = issues.check_comments_made(
-        TOKEN + "ac38d5bdd114df9a0ee8", "GatorEducator/gator", "gkapfham", 1, "all"
+        TOKEN + "ac38d5bdd114df9a0ee8", "GatorEducator/gator", "yeej2", 1, "all"
     )
     assert err == -2
