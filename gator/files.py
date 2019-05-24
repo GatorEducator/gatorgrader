d6d682544409dc77378f506a278a4259908bb521
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-24 19:11:06 -0400

    Add comments to the create_path function in the files module.

--- gator/files.py
+++ gator/files.py
@@ -10,11 +10,15 @@ def create_path(*args, file, home):
     # create the Path for the given file
     given_file_path = Path(file)
     final_path = home_path
-    # create a containing directory of sub-directories for the file
+    # Create a containing directory of sub-directories for the file.
+    # each of these paths will be a path between the home and the
+    # specified file. None of these paths need their anchor, though,
+    # which is given like "C:\" on Windows and "/" otherwise.
     # pylint: disable=old-division
     for containing_path in args:
         nested_path = Path(containing_path)
         final_path = final_path / nested_path.relative_to(nested_path.anchor)
+    # add the file at the end of the constructed file path
     final_path = final_path / given_file_path
     return final_path
 
