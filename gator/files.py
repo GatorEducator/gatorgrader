ee01bb84bbf8cd97cabd29b56d505150c118162a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-24 13:29:36 -0400

    Fix a bug in the check_file_in_directory function in the files module.

--- gator/files.py
+++ gator/files.py
@@ -1,26 +1,24 @@
 """Utility functions that check the contents of the file system"""
 
-import pathlib
 from pathlib import Path
-
-# FILE_SEPARATOR = "/"
 
 
 def check_file_in_directory(given_file, home_prefix, containing_directory):
     """Returns true if the specified file is in the directory"""
-    # OLD: file_for_checking = Path(containing_directory + FILE_SEPARATOR + given_file)
     # create the Path for the home_prefix
     home_prefix_path = Path(home_prefix)
-    print(str(home_prefix_path))
     # create the Path for the containing directory
     containing_directory_path = Path(containing_directory)
-    print(str(containing_directory_path))
     # create the Path for the given file
     given_file_path = Path(given_file)
-    print(str(given_file_path))
-    # create a file for checking using the two newly created paths
-    file_for_checking_path = home_prefix_path / containing_directory_path.relative_to(
-        containing_directory_path.anchor
+    # create the Path file for checking
+    # Since home_prefix_path and containing_directory_path are both absolute,
+    # the code must make the containing_directory_path relative by stripping
+    # off its anchor, allowing paths to be joined on all operating systems
+    file_for_checking_path = (
+        home_prefix_path
+        / containing_directory_path.relative_to(containing_directory_path.anchor)
+        / given_file_path
     )
     print("This is the file for checking: " + str(file_for_checking_path.absolute()))
     return file_for_checking_path.is_file()
