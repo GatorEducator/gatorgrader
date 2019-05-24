d6206cc9f329b7e998bfc994644d307db53361aa
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-24 15:39:30 -0400

    Add a commented-out debugging statement to count_entities in entities.

--- gator/entities.py
+++ gator/entities.py
@@ -22,6 +22,7 @@ def entity_greater_than_count(
 def count_entities(given_file, containing_directory, checking_function):
     """Counts the number of entities for the file in the directory"""
     file_for_checking = Path(containing_directory + FILE_SEPARATOR + given_file)
+    # print("File for checking" + str(file_for_checking))
     file_contents_count = 0
     if file_for_checking.is_file():
         file_contents = file_for_checking.read_text()
