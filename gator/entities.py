7b2d9d773e43d68f69bd778c004c32d5efcb4a94
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-24 21:00:39 -0400

    Add call to the create_path function from files in the entities module.

--- gator/entities.py
+++ gator/entities.py
@@ -9,6 +9,7 @@ def entity_greater_than_count(
     given_file, containing_directory, expected_count, checking_function, exact=False
 ):
     """Return count and determines if the entity count is greater than expected"""
+    # call the count_entities function in this module
     file_entity_count = count_entities(
         given_file, containing_directory, checking_function
     )
@@ -18,9 +19,10 @@ def entity_greater_than_count(
 
 def count_entities(given_file, containing_directory, checking_function):
     """Counts the number of entities for the file in the directory"""
-    # file_for_checking = Path(containing_directory + FILE_SEPARATOR + given_file)
+    # create a path for the given file and its containing directory
+    # note that this call does not specify any *args and thus there
+    # are no directories between the home directory and the file
     file_for_checking = files.create_path(file=given_file, home=containing_directory)
-    # print("File for checking" + str(file_for_checking))
     file_contents_count = 0
     if file_for_checking.is_file():
         file_contents = file_for_checking.read_text()
