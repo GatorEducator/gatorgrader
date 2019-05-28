24f74629dd2256ee450738f8494954af6957e4cd
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-28 17:32:39 -0400

    Add the starting version of the constants module.

---
+++ gator/constants.py
@@ -0,0 +1,17 @@
+"""Provide constants for reference by GatorGrader modules"""
+
+import collections
+import itertools
+
+
+def create_constants(name, *args, **kwargs):
+    """Create a namedtuple of constants"""
+    # the constants are created such that:
+    # the name is the name of the namedtuple
+    # for *args or **kwargs ConstantName = "ConstantName"
+    created_constants = collections.namedtuple(name, itertools.chain(args, kwargs.keys()))
+    return created_constants(*itertools.chain(args, kwargs.values()))
+
+
+# define the programming languages for comment checks
+LANGUAGES = create_constants('LANGUAGES', 'Java', 'Python')
