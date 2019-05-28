4d6a9c2fa6212b3c308b7af360c6961164588f09
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-28 17:49:30 -0400

    Change name of function in files so that it is platform-based.

--- gator/files.py
+++ gator/files.py
@@ -26,7 +26,7 @@ def create_path(*args, file, home):
     return final_path
 
 
-def case_insensitive_check_file_in_directory(*args, file, home):
+def platform_check_file_in_directory(*args, file, home):
     """Returns true if the case-insensitive specified file is in the directory"""
     # create the path so that it has this structure:
     # <home> + <any paths in *args, without their anchor> + <file>
@@ -58,7 +58,7 @@ def check_file_in_directory(*args, file, home):
     """Returns true if the specified file is in the directory"""
     # perform the standard check that relies on the operating system
     # to determine whether or not the file exists on the file system
-    no_case_file_exists = case_insensitive_check_file_in_directory(
+    no_case_file_exists = platform_check_file_in_directory(
         *args, file=file, home=home
     )
     # perform a case-sensitive check that lists all of the files in
