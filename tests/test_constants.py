b26c07f529199834dc41ab41e2f7f55ec53b75c1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-28 18:55:36 -0400

    Add tests to test_constants suite for the new markers constant variable.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -11,9 +11,29 @@ def test_language_constant_defined():
     assert constants.languages.Java == "Java"
 
 
+def test_marker_constant_defined():
+    """Check correctness for the variables in the marker constant"""
+    assert constants.markers.Empty == b""
+    assert constants.markers.No_Diagnostic == ""
+    assert constants.markers.Nothing == ""
+    assert constants.markers.Space == " "
+
+
 def test_language_constant_cannot_redefine():
-    """Check cannot redefine the variables in the language constant"""
+    """Check cannot redefine the variables in the languages constant"""
     with pytest.raises(AttributeError):
         constants.languages.Java = "cannot_set_constant"
     with pytest.raises(AttributeError):
         constants.languages.Python = "cannot_set_constant"
+
+
+def test_marker_constant_cannot_redefine():
+    """Check cannot redefine the variables in the markers constant"""
+    with pytest.raises(AttributeError):
+        constants.markers.Empty = "cannot_set_constant"
+    with pytest.raises(AttributeError):
+        constants.markers.No_Diagnostic = "cannot_set_constant"
+    with pytest.raises(AttributeError):
+        constants.markers.Nothing = "cannot_set_constant"
+    with pytest.raises(AttributeError):
+        constants.markers.Space = "cannot_set_constant"
