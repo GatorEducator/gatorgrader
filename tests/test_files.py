98daf2fecf7d6ebee0d855b49f6d2b1fc13eef52
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-28 11:33:47 -0400

    Add test to test_files test suite to start case-sensitive file name.

--- tests/test_files.py
+++ tests/test_files.py
@@ -49,7 +49,7 @@ def test_create_one_file_path_with_two_middle(tmpdir):
 
 
 def test_one_file_found_in_subdirectory(tmpdir):
-    """Ensure that check_file_in_directory can find one file file in a subdirectory"""
+    """Ensure that check_file_in_directory can find one file in a subdirectory"""
     hello_file = tmpdir.mkdir("sub").join("hello.txt")
     hello_file.write("content")
     assert hello_file.read() == "content"
@@ -60,8 +60,26 @@ def test_one_file_found_in_subdirectory(tmpdir):
     assert was_file_found is True
 
 
+def test_one_file_found_in_subdirectory_case_sensitivity_name(tmpdir):
+    """Ensure that check_file_in_directory can find case-sensitive file name in a subdirectory"""
+    hello_file = tmpdir.mkdir("sub").join("hello.txt")
+    hello_file.write("content")
+    assert hello_file.read() == "content"
+    assert len(tmpdir.listdir()) == 1
+    # the file should be found with the lowercase name
+    was_file_found = files.check_file_in_directory(
+        tmpdir.basename, "sub", file="hello.txt", home=tmpdir.dirname
+    )
+    assert was_file_found is True
+    # the file should be not found with the uppercase name
+    was_file_found = files.check_file_in_directory(
+        tmpdir.basename, "sub", file="HELLO.txt", home=tmpdir.dirname
+    )
+    assert was_file_found is False
+
+
 def test_many_files_found_in_subdirectory(tmpdir):
-    """Ensure that check_file_in_directory can find many files file in a subdirectory"""
+    """Ensure that check_file_in_directory can find many files in a subdirectory"""
     hello_file = tmpdir.mkdir("sub").join("hello.txt")
     hello_file.write("content")
     assert hello_file.read() == "content"
