b7b340039300b18779115c656759f531a677bbc8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-05-29 14:32:47 -0400

    Use the constants and add comments to the get_code function in leave.

--- gator/leave.py
+++ gator/leave.py
@@ -1,8 +1,14 @@
 """Determines the correct exit codes to help GatorGrader quit"""
 
+from gator import constants
+
 
 def get_code(return_values):
     """Get the correct exit code for all of the return values"""
+    # one of the return values is False,
+    # meaning that this is an error code
     if False in return_values:
-        return 1
-    return 0
+        return constants.codes.Error
+    # none of the return values are False,
+    # meaning that this is a success code
+    return constants.codes.Success
