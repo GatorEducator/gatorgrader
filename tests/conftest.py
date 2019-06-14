eeb5b345540ec3f4c0f14d562150313fb5e8255e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-06-13 22:49:47 -0400

    Delete commented-out code from the conftest file used for all tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,3 @@ GO_BACK_A_DIRECTORY = "/../"
 # set the system path to contain the previous directory
 PREVIOUS_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, PREVIOUS_DIRECTORY + GO_BACK_A_DIRECTORY)
-
-# # set the system path to contain the previous directory
-# mypath = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, mypath + '/../')
