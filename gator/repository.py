301359e25b3714078f2c8a2b6efe2c1aa9abefeb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-06-17 11:08:40 -0400

    Add the is_git_repository function and use it in repository.

--- gator/repository.py
+++ gator/repository.py
@@ -1,17 +1,38 @@
 """Interact with a Git repository."""
 
-from git import Repo
+import git
 
+from gator import constants
 from gator import util
 
 
-def get_commmits(path):
-    """Return a list of the commits for the repo at path."""
-    student_repository = Repo(path)
-    # pass in None so that the default (the current branch) is used
-    # this avoids problems with being checked out in different branches
-    # alternatively, we could detect the current HEAD and use that
-    commits = list(student_repository.iter_commits())
+def is_git_repository(repository_path):
+    """Return True if repository_path contains a Git repository, False otherwise."""
+    # attempt to create a repository using GitPython
+    try:
+        # it was possible to create the repository, so it must exist
+        # this means that the function should return True
+        # because it is safe to inspect attributes of this repository
+        _ = git.Repo(repository_path).git_dir
+        return True
+    # it was not possible to create the repository, so it does not exist
+    # this means that the function should return False
+    # because it is not safe to inspect Git attributes in this non-repo
+    except git.exc.InvalidGitRepositoryError:
+        return False
+
+
+def get_commmits(repository_path):
+    """Return a list of the commits for the repository at the path."""
+    # assume that we have not found a Git repository
+    # and thus set the default number of commits to zero
+    commits = constants.versioncontrol.No_Commits
+    if is_git_repository(repository_path):
+        student_repository = git.Repo(repository_path)
+        # pass in None so that the default (the current branch) is used
+        # this avoids problems with being checked out in different branches
+        # alternatively, we could detect the current HEAD and use that
+        commits = list(student_repository.iter_commits())
     return commits
 
 
