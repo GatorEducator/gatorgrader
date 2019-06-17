d648b165f53d25bcb7520f06d4827718e62513a9
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-06-16 23:26:27 -0400

    Fix double-period mistakes in docstrings of the test_arguments.

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -15,19 +15,19 @@ NOT_VERIFIED = False
 
 @pytest.fixture
 def no_gg_args():
-    """Return no command-line arguments.."""
+    """Return no command-line arguments."""
     return []
 
 
 @pytest.fixture
 def verifiable_gg_args():
-    """Return arguments that are verifiable.."""
+    """Return arguments that are verifiable."""
     return ["--directory", "D", "--file", "a", "--exists"]
 
 
 # pylint: disable=redefined-outer-name
 def test_default_argument_values_correct(no_gg_args):
-    """The default command-line arguments are correct.."""
+    """The default command-line arguments are correct."""
     gg_arguments = arguments.parse(no_gg_args)
     arguments_args_verified = arguments.verify(gg_arguments)
     assert arguments_args_verified == NOT_VERIFIED
@@ -35,7 +35,7 @@ def test_default_argument_values_correct(no_gg_args):
 
 # pylint: disable=redefined-outer-name
 def test_arguments_verified(verifiable_gg_args):
-    """Run arguments with verifiable arguments and it is verified.."""
+    """Run arguments with verifiable arguments and it is verified."""
     gg_arguments = arguments.parse(verifiable_gg_args)
     gg_args_verified = arguments.verify(gg_arguments)
     assert gg_args_verified == VERIFIED
@@ -136,7 +136,7 @@ def test_module_argument_not_verifiable_syserror(chosen_arguments, capsys):
     ],
 )
 def test_invalid_argument_combinations_not_accepted(chosen_arguments):
-    """Check that not valid argument combinations do not verify correctly.."""
+    """Check that not valid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.verify(parsed_arguments)
     assert verified_arguments is False
@@ -280,7 +280,7 @@ def test_valid_argument_combinations_accepted(chosen_arguments):
     ],
 )
 def test_invalid_argument_combinations_accepted(chosen_arguments):
-    """Check that not valid argument combinations do not verify correctly.."""
+    """Check that not valid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.verify(parsed_arguments)
     assert verified_arguments is False
@@ -301,7 +301,7 @@ def test_invalid_argument_combinations_accepted(chosen_arguments):
     ],
 )
 def test_is_valid_file_not_valid(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_file_and_directory(parsed_arguments)
     assert verified_arguments is False
@@ -319,7 +319,7 @@ def test_is_valid_file_not_valid(chosen_arguments):
     ],
 )
 def test_is_valid_file_or_directory_not_valid(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_file_or_directory(parsed_arguments)
     assert verified_arguments is False
@@ -342,7 +342,7 @@ def test_is_valid_file_or_directory_not_valid(chosen_arguments):
     ],
 )
 def test_is_not_valid_file_not_valid_comments_wrong(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_comments(parsed_arguments)
     assert verified_arguments is False
@@ -362,7 +362,7 @@ def test_is_not_valid_file_not_valid_comments_wrong(chosen_arguments):
     ],
 )
 def test_is_not_valid_file_not_valid_paragraphs_wrong(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_paragraphs(parsed_arguments)
     assert verified_arguments is False
@@ -382,7 +382,7 @@ def test_is_not_valid_file_not_valid_paragraphs_wrong(chosen_arguments):
     ],
 )
 def test_is_not_valid_file_not_valid_words_wrong(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_words(parsed_arguments)
     assert verified_arguments is False
@@ -432,7 +432,7 @@ def test_is_not_valid_file_not_valid_words_wrong(chosen_arguments):
     ],
 )
 def test_is_not_valid_language_combinations_wrong(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_language(parsed_arguments)
     assert verified_arguments is False
@@ -451,7 +451,7 @@ def test_is_not_valid_language_combinations_wrong(chosen_arguments):
     ],
 )
 def test_is_invalid_fragment_with_file_or_command(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_fragment(parsed_arguments)
     assert verified_arguments is False
@@ -468,7 +468,7 @@ def test_is_invalid_fragment_with_file_or_command(chosen_arguments):
     ],
 )
 def test_is_invalid_count_with_file_or_command(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_count(parsed_arguments)
     assert verified_arguments is False
@@ -484,7 +484,7 @@ def test_is_invalid_count_with_file_or_command(chosen_arguments):
     ],
 )
 def test_is_not_command_ancillary(chosen_arguments):
-    """Check that file ancillary detection verifies correctly.."""
+    """Check that file ancillary detection verifies correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     # note that this function is only checking for the presence
     # of the ancillary arguments like fragment and count
@@ -497,7 +497,7 @@ def test_is_not_command_ancillary(chosen_arguments):
     [(["--nowelcome", "--directory", "D", "--single", "2", "--exact"])],
 )
 def test_exact_count_check_not_valid(chosen_arguments):
-    """Check that invalid argument combinations do not verify correctly.."""
+    """Check that invalid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_exact(parsed_arguments)
     assert verified_arguments is False
@@ -518,7 +518,7 @@ def test_exact_count_check_not_valid(chosen_arguments):
     ],
 )
 def test_is_valid_file_valid(chosen_arguments):
-    """Check that valid argument combinations do not verify correctly.."""
+    """Check that valid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_file_and_directory(parsed_arguments)
     assert verified_arguments is True
@@ -540,7 +540,7 @@ def test_is_valid_file_valid(chosen_arguments):
     ],
 )
 def test_is_valid_comments_valid(chosen_arguments):
-    """Check that valid argument combinations do not verify correctly.."""
+    """Check that valid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_comments(parsed_arguments)
     assert verified_arguments is True
@@ -608,7 +608,7 @@ def test_is_valid_comments_valid(chosen_arguments):
     ],
 )
 def test_is_valid_comments_and_language_valid(chosen_arguments):
-    """Check that valid argument combinations do not verify correctly.."""
+    """Check that valid argument combinations do not verify correctly."""
     parsed_arguments = arguments.parse(chosen_arguments)
     verified_arguments = arguments.is_valid_language(parsed_arguments)
     assert verified_arguments is True
