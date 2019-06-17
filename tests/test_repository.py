0bdc68e48e7d9ae03effe80f62b67318a3a701cf
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-06-17 11:18:08 -0400

    Add tests to test_repository to start testing to Git detection feature.

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -2,18 +2,20 @@
 
 import sys
 
+from git import Repo
+
 from gator import repository
 
 
 def test_repository_not_zero_commits():
     """Check to ensure that GatorGrader's repository registers."""
-    commits = repository.get_commmits(".")
+    commits = repository.get_commits(".")
     assert len(commits) > 1
 
 
 def test_repository_not_zero_commits_extra_method():
     """Check to ensure that GatorGrader's repository registers."""
-    commits = repository.get_commmits(".")
+    commits = repository.get_commits(".")
     assert repository.count_commits(commits) > 1
 
 
@@ -39,3 +41,29 @@ def test_repository_commits_not_huge_exacted():
     """Check to ensure that commit counts work correctly."""
     valid, __ = repository.commits_greater_than_count(".", sys.maxsize, True)
     assert valid is False
+
+
+def test_detect_not_git_repository(tmpdir):
+    """Ensure that it is possible to detect that a directory is not a Git repository."""
+    # by default, the tmpdir is not a Git repository
+    # the check for the existence of a repository should be False
+    detected_git_repository = repository.is_git_repository(str(tmpdir))
+    assert detected_git_repository is False
+
+
+def test_detect_git_repository(tmpdir):
+    """Ensure that it is possible to detect that a directory is not a Git repository."""
+    # create a Git repository using GitPython
+    _ = Repo.init(str(tmpdir))
+    # since the repository was created in the tmpdir
+    # the check for the existence of a repository should be True
+    detected_git_repository = repository.is_git_repository(str(tmpdir))
+    assert detected_git_repository is True
+
+
+def test_nocrash_when_not_repository(tmpdir):
+    """Ensure that a check not in a repository does not crash."""
+    # since there is, by default, no repository in tmpdir
+    # then a call to get_commits will will return an empty list
+    commits = repository.get_commits(str(tmpdir))
+    assert len(commits) == 0
