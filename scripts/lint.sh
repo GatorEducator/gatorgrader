6cdda9992d3ca621f07b0275aa2b4aa943813e7f
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-02 16:22:59 -0400

    Add the command to run radon in the lint.sh script (gives diagnostics).

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -16,11 +16,13 @@ else
     FILES=$(find . -type f -regextype posix-extended -regex '\./(gator|tests)/.*.py')
 fi
 
+# lint all of the Python source code files
 FILES="$FILES *.py"
 
 declare -A LINTERS
 
-LINTERS=( ["black"]="pipenv run black $CHECK $FILES" ["pylint"]="pipenv run pylint $FILES" ["flake8"]="pipenv run flake8 $FILES" ["bandit"]="pipenv run bandit -c bandit.yml $FILES" ["pydocstyle"]="pipenv run pydocstyle $FILES" )
+# define all of the linters to iteratively run
+LINTERS=( ["black"]="pipenv run black $CHECK $FILES" ["pylint"]="pipenv run pylint $FILES" ["flake8"]="pipenv run flake8 $FILES" ["bandit"]="pipenv run bandit -c bandit.yml $FILES" ["radon"]="pipenv run radon mi $FILES" ["pydocstyle"]="pipenv run pydocstyle $FILES" )
 
 for tool in "${!LINTERS[@]}"; do
     echo " -- Running $tool"
