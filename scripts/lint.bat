feab22efb5b28120fc8ee51bd614242bca1b2726
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-02 17:11:49 -0400

    Add the integration with xenon and radon to the lint.bat file.

--- scripts/lint.bat
+++ scripts/lint.bat
@@ -13,6 +13,8 @@ dir /b /s | findstr "\\*.py$" | findstr /v "\\\.venv\\" > .xyzfiles
 for /f "Tokens=* Delims=" %%x in (.xyzfiles) do set FILES=!FILES! %%x
 del .xyzfiles
 
+set PATHS="gator tests"
+
 echo -- Running black
 pipenv run black %CHECK% %FILES%
 if ERRORLEVEL 1 (
@@ -51,6 +53,24 @@ if ERRORLEVEL 1 (
 
 echo -- Running pydocstyle
 pipenv run pydocstyle %FILES%
+if ERRORLEVEL 1 (
+    echo -- Failed
+    set PASSED=false
+) else (
+    echo -- Passed
+)
+
+echo -- Running radon
+pipenv run radon mi %FILES%
+if ERRORLEVEL 1 (
+    echo -- Failed
+    set PASSED=false
+) else (
+    echo -- Passed
+)
+
+echo -- Running xenon
+pipenv run xenon --max-absolute D --max-modules B --max-average B -c %PATHS%
 if ERRORLEVEL 1 (
     echo -- Failed
     set PASSED=false
