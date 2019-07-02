640243a2f3e1b50ea908d6dbbd370bef29ba0dbf
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-02 17:00:50 -0400

    Add comments and radon and xenon to the lint.sh file.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# determine whether or not black code formatting
+# will fail if code is not correctly formatted
+# "--check" returns non-zero exit code if formatting needed
+# otherwise, the black check is run for diagnostic purposes
 if [[ "$1" == "--check" ]]; then
     CHECK="--check"
 else
     CHECK=""
 fi
 
+# assume that all of the linters passed and prove otherwise
 PASSED=true
 
 OS="$(uname)"
 
+# collect the files on MacOS
 if [[ "$OS" == "Darwin" ]]; then
     FILES=$(find -E . -type f -regex '\./(gator|tests)/.*.py')
 else
@@ -19,11 +25,14 @@ fi
 # lint all of the Python source code files
 FILES="$FILES *.py"
 
-declare -A LINTERS
+# xenon cannot accept a lists of files, so give paths instead
+PATHS="gator tests"
 
 # define all of the linters to iteratively run
-LINTERS=( ["black"]="pipenv run black $CHECK $FILES" ["pylint"]="pipenv run pylint $FILES" ["flake8"]="pipenv run flake8 $FILES" ["bandit"]="pipenv run bandit -c bandit.yml $FILES" ["radon"]="pipenv run radon mi $FILES" ["pydocstyle"]="pipenv run pydocstyle $FILES" )
+declare -A LINTERS
+LINTERS=( ["black"]="pipenv run black $CHECK $FILES" ["pylint"]="pipenv run pylint $FILES" ["flake8"]="pipenv run flake8 $FILES" ["bandit"]="pipenv run bandit -c bandit.yml $FILES" ["radon"]="pipenv run radon mi $FILES" ["xenon"]="xenon --max-absolute D --max-modules B --max-average B -c $PATHS" ["pydocstyle"]="pipenv run pydocstyle $FILES" )
 
+# run each of the already configured linters
 for tool in "${!LINTERS[@]}"; do
     echo " -- Running $tool"
     # shellcheck disable=SC2086
@@ -36,6 +45,7 @@ for tool in "${!LINTERS[@]}"; do
     echo ""
 done
 
+# display the final diagnostic information
 if [[ "$PASSED" != "true" ]]; then
     echo "Not all linters passed!"
     exit 1
