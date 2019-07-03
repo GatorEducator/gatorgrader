178038f084cebf3be3061e10a347322145cdb7f3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-03 16:41:52 -0400

    chore: Add the new lint.sh code to run radon and xenon.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
+# determine whether or not black code formatting
+# will fail if code is not correctly formatted
+# "--check" returns non-zero exit code if formatting needed
+# otherwise, the black check is run for diagnostic purposes
 if [[ "$1" == "--check" ]]; then
     CHECK="--check"
 else
     CHECK=""
 fi
 
+# assume that all of the linters passed and prove otherwise
 PASSED=true
 
 OS="$(uname)"
 
+# collect the files on MacOS
 if [[ "$OS" == "Darwin" ]]; then
     FILES=$(find -E . -type f -regex '\./(gator|tests)/.*.py')
 else
     FILES=$(find . -type f -regextype posix-extended -regex '\./(gator|tests)/.*.py')
 fi
 
+# lint all of the Python source code files
 FILES="$FILES *.py"
 
+# xenon cannot accept a lists of files, so give paths instead
+PATHS="gator tests"
+
+# Notes about the linters:
+# - black checks and fixes Python code formatting
+# - pylint and flake8 check Python code
+# - bandit finds security problems in Python code
+# - radon checks code quality for diagnostic purposes
+# - xenon returns an error code for code quality thresholds
+
+# define all of the linters to iteratively run
 declare -A LINTERS
+LINTERS=( ["black"]="pipenv run black $CHECK $FILES" ["pylint"]="pipenv run pylint $FILES" ["flake8"]="pipenv run flake8 $FILES" ["bandit"]="pipenv run bandit -c bandit.yml $FILES" ["radon"]="pipenv run radon mi $FILES" ["xenon"]="pipenv run xenon --max-absolute D --max-modules B --max-average B -c $PATHS" ["pydocstyle"]="pipenv run pydocstyle $FILES" )
 
-LINTERS=( ["black"]="pipenv run black $CHECK $FILES" ["pylint"]="pipenv run pylint $FILES" ["flake8"]="pipenv run flake8 $FILES" ["bandit"]="pipenv run bandit -c bandit.yml $FILES" ["pydocstyle"]="pipenv run pydocstyle $FILES" )
-
+# run each of the already configured linters
 for tool in "${!LINTERS[@]}"; do
     echo " -- Running $tool"
     # shellcheck disable=SC2086
@@ -34,6 +52,7 @@ for tool in "${!LINTERS[@]}"; do
     echo ""
 done
 
+# display the final diagnostic information
 if [[ "$PASSED" != "true" ]]; then
     echo "Not all linters passed!"
     exit 1
