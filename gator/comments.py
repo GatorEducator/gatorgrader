afd50b73a0f72aac913733b52bd6c7a981c77cc5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-04 14:06:21 -0400

    style: Fix a spelling mistake in a comment in comments module.

--- gator/comments.py
+++ gator/comments.py
@@ -17,7 +17,7 @@ MULTILINECOMMENT_RE_PYTHON = r'^[ \t]*"""(.*?)"""[ \t]*$'
 
 # Note that all of these functions also return an empty dictionary
 # (i.e., {}) so that it matches the return signature of the
-# other analagous function that counts the words and returns
+# other analogous function that counts the words and returns
 # a dictionary of the {number of a paragraph, word count in paragraph}
 
 
