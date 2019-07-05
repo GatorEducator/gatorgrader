a5dc0f709e4d216702e2039a3749c82d8b0d5672
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-05 10:12:47 -0400

    feat: Improve the count_entities in entities to handle wildcards.

--- gator/entities.py
+++ gator/entities.py
@@ -21,22 +21,32 @@ def entity_greater_than_count(
 
 
 def count_entities(given_file, containing_directory, checking_function):
-    """Count the number of entities for the file in the directory."""
+    """Count the number of entities for the file(s) in the directory."""
+    # create an empty dictionary of filenames and an internal dictionary
+    file_counts_dictionary = {}
     # create a path for the given file and its containing directory
     # note that this call does not specify any *args and thus there
     # are no directories between the home directory and the file
-    file_for_checking = files.create_path(file=given_file, home=containing_directory)
-    # start the count of the number of entities at zero, assuming none found yet
-    file_contents_count = 0
-    # create an empty dictionary of the counts
-    file_contents_count_dictionary = {}
-    # a valid file exists and thus it is acceptable to perform the checking
-    if file_for_checking.is_file():
-        # extract the text from the file_for_checking
-        file_contents = file_for_checking.read_text()
-        # use the provided checking_function to check the contents of the file
-        # note this works since Python supports passing a function to a function
-        file_contents_count, file_contents_count_dictionary = checking_function(
-            file_contents
-        )
-    return file_contents_count, file_contents_count_dictionary
+    for file_for_checking in files.create_paths(
+        file=given_file, home=containing_directory
+    ):
+        # start the count of the number of entities at zero, assuming none found yet
+        file_contents_count = 0
+        # create an empty dictionary of the counts
+        file_contents_count_dictionary = {}
+        # a valid file exists and thus it is acceptable to perform the checking
+        if file_for_checking.is_file():
+            # extract the text from the file_for_checking
+            file_contents = file_for_checking.read_text()
+            # use the provided checking_function to check the contents of the file
+            # note this works since Python supports passing a function to a function
+            file_contents_count, file_contents_count_dictionary = checking_function(
+                file_contents
+            )
+            file_counts_dictionary[
+                file_for_checking.name
+            ] = file_contents_count_dictionary
+    file_contents_count_overall = util.get_first_minimum_value_deep(
+        file_counts_dictionary
+    )[1][1]
+    return file_contents_count_overall, file_counts_dictionary
