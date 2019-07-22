9c87fdc497bdbff1dea83d351ab562a4b7831b8b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-07-22 10:21:18 -0400

    chore: Add the switch task to the .tasks.py file and comment it.

--- .tasks.py
+++ .tasks.py
@@ -3,16 +3,28 @@
 from invoke import task
 
 
+@task
+def switch(c, pyenv="3.7.3"):
+    """Switch the version of Python managed by Pipenv to specified version."""
+    # select current_pyenv as the version of Python
+    c.run("pyenv local " + pyenv)
+    # create the virtualenv managed by Pipenv with current_pyenv
+    c.run("pipenv install --python=`pyenv which python` --dev")
+    # display diagnostic information about new version of Python
+    c.run("python --version")
+
+
 @task(iterable=['pyenv'])
 def test(c, pyenv):
     """Run tests with Pytest after full setup of each provided Pyenv version."""
+    # Note that this task will leave a developer in the last specified version of Python
     # run the test suite for all of the provided versions of Python managed by Pyenv
     for current_pyenv in pyenv:
         print("Python version: " + current_pyenv)
         # select current_pyenv as the version of Python
         c.run("pyenv local " + current_pyenv)
         # create the virtualenv managed by Pipenv with current_pyenv
-        c.run("yes | pipenv install --python=`pyenv which python` --dev")
+        c.run("pipenv install --python=`pyenv which python` --dev")
         # display diagnostic information about new version of Python
         c.run("python --version")
         # run the test suite and collect coverage information
