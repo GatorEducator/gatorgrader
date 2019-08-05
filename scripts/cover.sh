242f57ad8d763881bafbfea3cac2e622baf07133
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-05 15:18:08 -0400

    coms: Delete an incorrect comment inside of the cover.sh script.

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Run the test suite so that:
-# --> -x: Stops on first error or failure
 # --> -s: Outputs all diagnostic information
 # --> Other parameters: Track coverage and output report as XML file
 pipenv run pytest -s --cov-config .coveragerc --cov-report term-missing --cov-report xml --cov --cov-branch
