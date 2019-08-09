b98e88ef2b5f14d1df1fe2b9289e9a13e2f16205
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-09 17:58:52 -0400

    test: Add tests for the required check positional argument.

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -1,22 +1,13 @@
 """Tests for the input and checking of command-line arguments."""
 
+import argparse
 import pytest
 
 from gator import arguments
 
-EMPTY_STRING = ""
-ERROR = "error:"
 
 VERIFIED = True
 NOT_VERIFIED = False
-
-# Simple Fixture-based Tests {{{
-
-
-@pytest.fixture
-def no_gg_args():
-    """Return no command-line arguments."""
-    return []
 
 
 @pytest.fixture
@@ -25,18 +16,32 @@ def verifiable_gg_args():
     return ["check_commits"]
 
 
-def test_default_argument_values_correct(no_gg_args):
-    """The default command-line arguments are correct."""
-    gg_arguments = arguments.parse(no_gg_args)
-    arguments_args_verified = arguments.verify(gg_arguments)
-    assert arguments_args_verified == NOT_VERIFIED
+def test_no_arguments_incorrect_system_exit(capsys):
+    """No command-line arguments causes SystemExit crash of argparse with error output."""
+    with pytest.raises(SystemExit):
+        _ = arguments.parse([])
+    captured = capsys.readouterr()
+    # there is no standard output
+    counted_newlines = captured.out.count("\n")
+    assert counted_newlines == 0
+    # standard error has two lines from pytest
+    assert "usage:" in captured.err
+    counted_newlines = captured.err.count("\n")
+    assert counted_newlines == 2
 
 
-def test_arguments_verified(verifiable_gg_args):
+def test_no_arguments_incorrect_system_exit_not_verified(capsys):
+    """No command-line arguments causes SystemExit crash and is not verified."""
+    with pytest.raises(SystemExit):
+        gg_arguments = arguments.parse([])
+        arguments_args_verified = arguments.verify(gg_arguments)
+        assert arguments_args_verified == NOT_VERIFIED
+    # capture the output so that test output is not polluted
+    _ = capsys.readouterr()
+
+
+def test_basic_check_correct(verifiable_gg_args):
     """Run arguments with verifiable arguments and it is verified."""
     gg_arguments = arguments.parse(verifiable_gg_args)
     gg_args_verified = arguments.verify(gg_arguments)
     assert gg_args_verified == VERIFIED
-
-
-# }}}
