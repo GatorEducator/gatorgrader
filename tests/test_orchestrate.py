dd3ba526801e5157b29e8a158895a2abae1ab470
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-10 20:23:11 -0400

    refactor: Call the new perform_actions in the test_orchestrate.

--- tests/test_orchestrate.py
+++ tests/test_orchestrate.py
@@ -13,11 +13,28 @@ def reset_results_dictionary():
     report.reset()
 
 
+@pytest.mark.parametrize(
+    "commandline_arguments",
+    [
+        (["--json", "check_commits"]),
+        (["--json", "--nowelcome", "check_commits"]),
+        (["--nowelcome", "check_commits"]),
+    ],
+)
+def test_verify_arguments(commandline_arguments):
+    """Check if the verification of arguments works from orchestrate."""
+    parsed_arguments, verification_status = orchestrate.verify_arguments(
+        commandline_arguments
+    )
+    assert parsed_arguments is not None
+    assert verification_status is True
+
+
 def test_perform_actions_no_parameters_welcome(capsys):
     """Check to see if perform can invoke welcome action with no parameters."""
     actions = []
     actions.append([orchestrate.DISPLAY, "welcome_message", []])
-    orchestrate.perform(actions)
+    orchestrate.perform_actions(actions)
     captured = capsys.readouterr()
     counted_newlines = captured.out.count("\n")
     assert "GatorGrader" in captured.out
@@ -29,7 +46,7 @@ def test_perform_actions_no_parameters_incorrect(capsys):
     """Check to see if perform can invoke welcome action with no parameters."""
     actions = []
     actions.append([orchestrate.DISPLAY, "incorrect_message", []])
-    orchestrate.perform(actions)
+    orchestrate.perform_actions(actions)
     captured = capsys.readouterr()
     counted_newlines = captured.out.count("\n")
     assert "Incorrect" in captured.out
@@ -43,6 +60,6 @@ def test_perform_actions_single_parameter_exit(capsys):
     actions = []
     with pytest.raises(SystemExit) as pytest_wrapped_e:
         actions.append([orchestrate.RUN, "run_exit", [constants.arguments.Incorrect]])
-        orchestrate.perform(actions)
+        orchestrate.perform_actions(actions)
     assert pytest_wrapped_e.type == SystemExit
     assert pytest_wrapped_e.value.code == 2
