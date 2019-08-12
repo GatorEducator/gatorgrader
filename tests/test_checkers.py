ad0071f498dab263d7e3e409bb85ad90e4a4ec39
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-12 13:16:46 -0400

    test: Add test to test_checkers to handle [""] case for checker source.

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -58,6 +58,22 @@ def test_load_checkers_list_is_not_empty_default_input():
         assert "check_" in checker
 
 
+def test_load_checkers_list_is_not_empt_blank_input():
+    """Ensures checker loading results in non-empty list with defaults."""
+    # this case would occur on the command-line when
+    # the checkerdir is not specified as an argument
+    checker_source = checkers.get_source([""])
+    # the source for the plugins is not empty
+    assert checker_source is not None
+    # the source for the plugins contains plugins
+    # that adhere to the naming scheme "check_<ACTION><ENTITY>"
+    # for instance "check_commits"
+    # we know that this should be true because all
+    # internal GatorGrader plugins adhere to this convention
+    for checker in checker_source.list_plugins():
+        assert "check_" in checker
+
+
 def test_load_checkers_list_is_not_empty_provided_input(tmpdir):
     """Ensures checker loading results in non-empty list with provided list."""
     # create a single checker in a new directory
