7955398354f6593f643afb1e63995e4144500786
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-11 20:18:56 -0400

    test: Add checks for internal names and comments to the test_checkers.

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -6,5 +6,13 @@ from gator import checkers
 
 def test_load_checkers_list_is_not_empty_default_input():
     """Ensures checker loading results in non-empty list with defaults."""
-    checker_source_list = checkers.get_sources()
-    assert checker_source_list is not None
+    checker_source = checkers.get_source()
+    # the source for the plugins is not empty
+    assert checker_source is not None
+    # the source for the plugins contains plugins
+    # that adhere to the naming scheme "check_<ACTION>"
+    # for instance "check_commits"
+    # we know that this should be true because all
+    # internal GatorGrader plugins adhere to this convention
+    for checker in checker_source.list_plugins():
+        assert "check_" in checker
