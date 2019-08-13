b6435da5d3209197912c47e631a77cb946e1a5cb
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-13 19:43:45 -0400

    test: Update test_arguments to acknowledge new parsing scheme.

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -26,7 +26,7 @@ def test_no_arguments_incorrect_system_exit(capsys):
 def test_no_arguments_incorrect_system_exit_not_verified(capsys):
     """No command-line arguments causes SystemExit crash and is not verified."""
     with pytest.raises(SystemExit):
-        gg_arguments = arguments.parse([])
+        gg_arguments, remaining_arguments = arguments.parse([])
         arguments_args_verified = arguments.verify(gg_arguments)
         assert arguments_args_verified == NOT_VERIFIED
     # capture the output so that test output is not polluted
@@ -35,7 +35,7 @@ def test_no_arguments_incorrect_system_exit_not_verified(capsys):
 
 def test_basic_check_correct():
     """When given verifiable arguments, there is no error and it is verified."""
-    gg_arguments = arguments.parse(["check_commits"])
+    gg_arguments, remaining_arguments = arguments.parse(["check_commits"])
     args_verified = arguments.verify(gg_arguments)
     assert args_verified == VERIFIED
 
@@ -50,33 +50,9 @@ def test_basic_check_correct():
 )
 def test_optional_commandline_arguments_can_verify(commandline_arguments):
     """Check that correct optional command-line arguments check correctly."""
-    gg_arguments = arguments.parse(commandline_arguments)
+    gg_arguments, remaining_arguments = arguments.parse(commandline_arguments)
     args_verified = arguments.verify(gg_arguments)
     assert args_verified is True
-
-
-@pytest.mark.parametrize(
-    "commandline_arguments",
-    [
-        (["--jsonFF", "check_commits"]),
-        (["--json", "--nowelcomeFF", "check_commits"]),
-        (["--nowelcomeFF", "check_commits"]),
-        (["--checkerdir", "checker_directory"]),
-        (["--checkerdirFF", "checker_directory", "check_commits"]),
-    ],
-)
-def test_optional_commandline_arguments_cannot_verify(commandline_arguments, capsys):
-    """Check that incorrect optional command-line arguments check correctly."""
-    with pytest.raises(SystemExit):
-        _ = arguments.parse(commandline_arguments)
-    captured = capsys.readouterr()
-    # there is no standard output
-    counted_newlines = captured.out.count("\n")
-    assert counted_newlines == 0
-    # standard error has two lines from pytest
-    assert "usage:" in captured.err
-    counted_newlines = captured.err.count("\n")
-    assert counted_newlines == 2
 
 
 def test_checkerdir_is_valid_arguments_verify(tmpdir):
@@ -86,7 +62,7 @@ def test_checkerdir_is_valid_arguments_verify(tmpdir):
     # this directory exists on the file system and verification should work
     checker_directory = tmpdir.dirname + "/" + tmpdir.basename + "/" + "checks"
     commandline_arguments = ["--checkerdir", checker_directory, "check_messages"]
-    gg_arguments = arguments.parse(commandline_arguments)
+    gg_arguments, remaining_arguments = arguments.parse(commandline_arguments)
     args_verified = arguments.verify(gg_arguments)
     assert args_verified is True
 
@@ -98,6 +74,6 @@ def test_checkerdir_is_not_valid_arguments_verify(tmpdir):
     # this directory does not exist on the file system and verification should not work
     checker_directory = tmpdir.dirname + "/" + tmpdir.basename + "/" + "checksWRONG"
     commandline_arguments = ["--checkerdir", checker_directory, "check_messages"]
-    gg_arguments = arguments.parse(commandline_arguments)
+    gg_arguments, remaining_arguments = arguments.parse(commandline_arguments)
     args_verified = arguments.verify(gg_arguments)
     assert args_verified is False
