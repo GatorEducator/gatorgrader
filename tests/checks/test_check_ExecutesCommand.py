8bfa444626dc3fec3dc39bdec08d7c42dfdf3c0a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-14 11:13:24 -0400

    test: Add the test_check_ExecutesCommand test suite.

---
+++ tests/checks/test_check_ExecutesCommand.py
@@ -0,0 +1,47 @@
+"""Tests for ExecutesCommand's input and verification of command-line arguments."""
+
+import pytest
+
+from gator.checks import check_ExecutesCommand
+
+
+def test_no_arguments_incorrect_system_exit(capsys):
+    """No command-line arguments causes SystemExit crash of argparse with error output."""
+    with pytest.raises(SystemExit):
+        _ = check_ExecutesCommand.parse([])
+    captured = capsys.readouterr()
+    # there is no standard output
+    counted_newlines = captured.out.count("\n")
+    assert counted_newlines == 0
+    # standard error has two lines from pytest
+    assert "usage:" in captured.err
+    counted_newlines = captured.err.count("\n")
+    assert counted_newlines == 2
+
+
+@pytest.mark.parametrize(
+    "commandline_arguments",
+    [(["--commandWRONG", "echo"]), (["--command", "run", "--WRONG"]), (["--command"])],
+)
+def test_optional_commandline_arguments_cannot_verify(commandline_arguments, capsys):
+    """Check that incorrect optional command-line arguments check correctly."""
+    with pytest.raises(SystemExit):
+        _ = check_ExecutesCommand.parse(commandline_arguments)
+    captured = capsys.readouterr()
+    # there is no standard output
+    counted_newlines = captured.out.count("\n")
+    assert counted_newlines == 0
+    # standard error has two lines from pytest
+    assert "usage:" in captured.err
+    counted_newlines = captured.err.count("\n")
+    assert counted_newlines == 2
+
+
+@pytest.mark.parametrize(
+    "commandline_arguments",
+    [(["--command", "run_command_first"]), (["--command", "run_command_second"])],
+)
+def test_optional_commandline_arguments_can_verify(commandline_arguments, not_raises):
+    """Check that correct optional command-line arguments check correctly."""
+    with not_raises(SystemExit):
+        _ = check_ExecutesCommand.parse(commandline_arguments)
