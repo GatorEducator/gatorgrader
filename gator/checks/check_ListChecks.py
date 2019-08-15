f8ff9970b1985c5933c67122e1bcb23e18dbe2e8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-15 16:16:11 -0400

    feat: Fix the parse in check_ListChecks and improve act function.

--- gator/checks/check_ListChecks.py
+++ gator/checks/check_ListChecks.py
@@ -4,10 +4,12 @@ import argparse
 
 from gator import checkers
 
-# import snoop
-# snoop.install(color="rrt")
+import snoop
+
+snoop.install(color="rrt")
 
 
+@snoop
 def get_parser():
     """Get a parser for the arguments provided on the command-line."""
     # create the parser with the default help formatter
@@ -43,18 +45,25 @@ def get_parser():
 
 def parse(args, parser=None):
     """Use the parser on the provided arguments."""
-    checkers.parse(get_parser, args, parser)
+    return checkers.parse(get_parser, args, parser)
 
 
 def act(main_parsed_arguments, check_remaining_arguments):
     """Perform the action for this check."""
     check_parsed_arguments = parse(check_remaining_arguments)
-    print(check_parsed_arguments)
     # get the source of all the checkers available from either:
     # --> the internal directory of checkers (e.g., "./gator/checks")
     # --> the directory specified on the command-line
     external_checker_directory = checkers.get_checker_dir(main_parsed_arguments)
     checker_source = checkers.get_source([external_checker_directory])
-    # TODO: filter the names of checks with check_parsed_arguments?
-    help_messages = checkers.get_checks_help(checker_source)
+    # filter the names of checks with check_parsed_arguments?
+    if (
+        check_parsed_arguments is not None
+        and check_parsed_arguments.namecontains is not None
+    ):
+        help_messages = checkers.get_checks_help(
+            checker_source, check_parsed_arguments.namecontains
+        )
+    else:
+        help_messages = checkers.get_checks_help(checker_source)
     return help_messages
