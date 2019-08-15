51b37698a1ebd29514dce2935ebb360a7e5822fc
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-15 15:34:39 -0400

    feat: Add the starting implementation of act to check_ListChecks.

--- gator/checks/check_ListChecks.py
+++ gator/checks/check_ListChecks.py
@@ -4,6 +4,9 @@ import argparse
 
 from gator import checkers
 
+# import snoop
+# snoop.install(color="rrt")
+
 
 def get_parser():
     """Get a parser for the arguments provided on the command-line."""
@@ -30,7 +33,7 @@ def get_parser():
     optional_group.add_argument(
         "--namecontains",
         metavar="LABEL",
-        help="filter checks by a label that a check's name must contain",
+        help="filter by label that name must contain",
         type=str,
     )
 
@@ -41,3 +44,17 @@ def get_parser():
 def parse(args, parser=None):
     """Use the parser on the provided arguments."""
     checkers.parse(get_parser, args, parser)
+
+
+def act(main_parsed_arguments, check_remaining_arguments):
+    """Perform the action for this check."""
+    check_parsed_arguments = parse(check_remaining_arguments)
+    print(check_parsed_arguments)
+    # get the source of all the checkers available from either:
+    # --> the internal directory of checkers (e.g., "./gator/checks")
+    # --> the directory specified on the command-line
+    external_checker_directory = checkers.get_checker_dir(main_parsed_arguments)
+    checker_source = checkers.get_source([external_checker_directory])
+    # TODO: filter the names of checks with check_parsed_arguments?
+    help_messages = checkers.get_checks_help(checker_source)
+    return help_messages
