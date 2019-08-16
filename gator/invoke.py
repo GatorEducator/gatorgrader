cbbb3ef9fe033fe18f608c1ed45ebe1e7bfc0367
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-16 14:49:11 -0400

    refactor: Add imports to make it easy to use snoop in invoke module.

--- gator/invoke.py
+++ gator/invoke.py
@@ -11,6 +11,9 @@ from gator import repository
 from gator import run
 from gator import util
 
+import snoop
+snoop.install(color="rrt")
+
 
 def report_result(status, message, diagnostic):
     """Set the report after running a check."""
@@ -22,6 +25,7 @@ def report_result(status, message, diagnostic):
         report.set_result(message, status, diagnostic)
 
 
+@snoop
 def invoke_commits_check(student_repository, expected_count, exact=False):
     """Check to see if the repository has more than specified commits."""
     # inspect the Git repository internals for the commits
