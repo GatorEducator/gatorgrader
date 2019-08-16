0c4d3e687edfd2e4e7547d73b60f36e17c9e0e3a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-16 18:39:31 -0400

    feat: Add the act function for check_ExecutesCommand.

--- gator/checks/check_ExecutesCommand.py
+++ gator/checks/check_ExecutesCommand.py
@@ -3,6 +3,7 @@
 import argparse
 
 from gator import checkers
+from gator import invoke
 
 
 def get_parser():
@@ -38,3 +39,19 @@ def get_parser():
 def parse(args, parser=None):
     """Use the parser on the provided arguments."""
     return checkers.parse(get_parser, args, parser)
+
+
+# pylint: disable=unused-argument
+def act(main_parsed_arguments, check_remaining_arguments):
+    """Perform the action for this check."""
+    # extract the two arguments for this check:
+    # --> count is required to specify the commit count threshold
+    # --> exact is optional, but will either be True or False and False by default
+    check_parsed_arguments = parse(check_remaining_arguments)
+    # Directly run the check since at least one of the argument's for it is mandatory.
+    # This means that the use of check_CountCommits would have already failed by this
+    # point since argparse will exit the program if the command-line argument is not provided
+    command = check_parsed_arguments.command
+    return [
+        invoke.invoke_all_command_executes_checks(command)
+    ]
