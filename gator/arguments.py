5f2b677bce6169dbd04de9221813efabe649cd87
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-17 22:43:22 -0400

    feat: Add GatorGraderArgumentParser to the arguments module.

--- gator/arguments.py
+++ gator/arguments.py
@@ -1,20 +1,44 @@
 """Handle the top-level command-line arguments provided to GatorGrader."""
 
+from gator import checkers
 from gator import constants
+from gator import display
 from gator import files
 
 import argparse
 
-# import snoop
-# snoop.install(color="rrt")
+
+class GatorGraderArgumentParser(argparse.ArgumentParser):
+    """Create a subclass of ArgumentParser that improves the help display."""
+
+    def print_help(self, status=1, message=None):
+        """Print the standard help message and then list the checkers."""
+        # display the standard help message
+        super().print_help()
+        # Get all of the checks available through pluginbase.
+        # Note that the "--checkdir" is not available because
+        # when argparse recognizes "--help" the parse is not
+        # completed. This means that this function can only
+        # display those internal checks through pluginbase.
+        checker_source = checkers.get_source()
+        help_messages = checkers.get_checks_help(
+            checker_source, indent=constants.markers.Indent
+        )
+        # display a blank line, a label, and then the collected
+        # help messages from the internal checks through pluginbase
+        display.line()
+        display.line("internal checks:")
+        display.line(help_messages)
 
 
 def parse(args):
     """Parse the arguments provided on the command-line."""
     # create the parser with the raw test help formatter
     # that will maintain the newlines and spaces in the epilog
-    parser = argparse.ArgumentParser(
-        prog="gatorgrader.py", formatter_class=argparse.RawTextHelpFormatter
+    # parser = argparse.ArgumentParser(
+    parser = GatorGraderArgumentParser(
+        prog="gatorgrader.py",
+        formatter_class=argparse.RawTextHelpFormatter
     )
 
     # assign a label to indicate that there is only one
@@ -67,7 +91,7 @@ def parse(args):
 
     # add an epilog to explain how to list all of the available checks,
     # including those checks that are internal and user-provided
-    parser.epilog = "check listing:\r\n  list all available checks with CHECK as ListChecks\r\n  usage: gatorgrader.py ListChecks"
+    parser.epilog = "all checks:\r\n  list all available checks with CHECK as ListChecks\r\n  usage: gatorgrader.py ListChecks"
     # call argparse's parse_known_args function that will recognize all
     # matching arguments and those that remain to be parsed and return them both
     arguments_finished, arguments_remaining = parser.parse_known_args(args)
