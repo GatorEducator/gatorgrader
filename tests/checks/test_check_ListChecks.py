ce6e3599b415ee53cb84d52d4d65149eaca60ad8
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-22 16:02:40 -0400

    test: Add a test and improve a test in test_check_ListChecks.

--- tests/checks/test_check_ListChecks.py
+++ tests/checks/test_check_ListChecks.py
@@ -1,6 +1,10 @@
 """Tests for ListCheck's input and verification of command-line arguments."""
 
+import os
 import pytest
+import sys
+
+from unittest.mock import patch
 
 from gator import arguments
 from gator import checkers
@@ -83,23 +87,66 @@ def test_optional_commandline_arguments_can_parse_created_parser(
 )
 def test_act_produces_output(commandline_arguments):
     """Check that using the check produces output."""
-    parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
-    args_verified = arguments.verify(parsed_arguments)
-    assert args_verified is True
-    external_checker_directory = checkers.get_checker_dir(parsed_arguments)
-    checker_source = checkers.get_source([external_checker_directory])
-    check_name = checkers.get_chosen_check(parsed_arguments)
-    check_file = checkers.transform_check(check_name)
-    check_exists = checkers.verify_check_existence(check_file, checker_source)
-    assert check_exists is True
-    check = checker_source.load_plugin(check_file)
-    check_result = check.act(parsed_arguments, remaining_arguments)
-    # the result is True
-    assert check_result is not None
-    assert len(check_result) == 1
-    assert check_result[0] is True
-    # the report contains expected results
-    assert report.get_result() is not None
-    assert len(report.get_result()["check"]) > 1
-    assert report.get_result()["outcome"] is True
-    assert report.get_result()["diagnostic"] == ""
+    testargs = [os.getcwd()]
+    with patch.object(sys, "argv", testargs):
+        parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
+        args_verified = arguments.verify(parsed_arguments)
+        assert args_verified is True
+        external_checker_directory = checkers.get_checker_dir(parsed_arguments)
+        checker_source = checkers.get_source([external_checker_directory])
+        check_name = checkers.get_chosen_check(parsed_arguments)
+        check_file = checkers.transform_check(check_name)
+        check_exists = checkers.verify_check_existence(check_file, checker_source)
+        assert check_exists is True
+        check = checker_source.load_plugin(check_file)
+        check_result = check.act(parsed_arguments, remaining_arguments)
+        # the result is True
+        assert check_result is not None
+        assert len(check_result) == 1
+        assert check_result[0] is True
+        # the report contains expected results
+        assert report.get_result() is not None
+        assert len(report.get_result()["check"]) > 1
+        assert report.get_result()["outcome"] is True
+        assert report.get_result()["diagnostic"] == ""
+
+
+@pytest.mark.parametrize(
+    "commandline_arguments",
+    [
+        (["ListChecks", "--namecontains", "NotValidCheckName"]),
+        (
+            [
+                "--checkerdir",
+                "./gator/checks",
+                "ListChecks",
+                "--namecontains",
+                "NotValidCheckName",
+            ]
+        ),
+    ],
+)
+def test_act_produces_output_invalid_check_name(commandline_arguments):
+    """Check that using the check produces output."""
+    testargs = [os.getcwd()]
+    with patch.object(sys, "argv", testargs):
+        parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
+        args_verified = arguments.verify(parsed_arguments)
+        assert args_verified is True
+        external_checker_directory = checkers.get_checker_dir(parsed_arguments)
+        checker_source = checkers.get_source([external_checker_directory])
+        check_name = checkers.get_chosen_check(parsed_arguments)
+        check_file = checkers.transform_check(check_name)
+        check_exists = checkers.verify_check_existence(check_file, checker_source)
+        assert check_exists is True
+        check = checker_source.load_plugin(check_file)
+        check_result = check.act(parsed_arguments, remaining_arguments)
+        # the result is True
+        assert check_result is not None
+        assert len(check_result) == 1
+        assert check_result[0] is False
+        # the report contains expected results
+        assert report.get_result() is not None
+        assert len(report.get_result()["check"]) > 1
+        assert report.get_result()["outcome"] is False
+        assert report.get_result()["diagnostic"] != ""
