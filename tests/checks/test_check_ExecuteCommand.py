91e08c805225efbf3a5597b8fb4d2df9527c8a6c
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-22 16:20:49 -0400

    test: Use mock to make test_check_ExecuteCommand work in more ways.

--- tests/checks/test_check_ExecuteCommand.py
+++ tests/checks/test_check_ExecuteCommand.py
@@ -1,6 +1,11 @@
 """Tests for ExecuteCommand's input and verification of command-line arguments."""
 
 import pytest
+import os
+import sys
+
+from unittest.mock import patch
+
 
 from gator import arguments
 from gator import checkers
@@ -72,26 +77,28 @@ def test_optional_commandline_arguments_can_parse_created_parser(
 )
 def test_act_produces_output(commandline_arguments, expected_result):
     """Check that using the check produces output."""
-    parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
-    args_verified = arguments.verify(parsed_arguments)
-    assert args_verified is True
-    external_checker_directory = checkers.get_checker_dir(parsed_arguments)
-    checker_source = checkers.get_source([external_checker_directory])
-    check_name = checkers.get_chosen_check(parsed_arguments)
-    check_file = checkers.transform_check(check_name)
-    check_exists = checkers.verify_check_existence(check_file, checker_source)
-    assert check_exists is True
-    check = checker_source.load_plugin(check_file)
-    check_result = check.act(parsed_arguments, remaining_arguments)
-    # check the result
-    assert check_result is not None
-    assert len(check_result) == 1
-    assert check_result[0] is expected_result
-    # check the contents of the report
-    assert report.get_result() is not None
-    assert len(report.get_result()["check"]) > 1
-    assert report.get_result()["outcome"] is expected_result
-    if expected_result:
-        assert report.get_result()["diagnostic"] == ""
-    else:
-        assert report.get_result()["diagnostic"] != ""
+    testargs = [os.getcwd()]
+    with patch.object(sys, "argv", testargs):
+        parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
+        args_verified = arguments.verify(parsed_arguments)
+        assert args_verified is True
+        external_checker_directory = checkers.get_checker_dir(parsed_arguments)
+        checker_source = checkers.get_source([external_checker_directory])
+        check_name = checkers.get_chosen_check(parsed_arguments)
+        check_file = checkers.transform_check(check_name)
+        check_exists = checkers.verify_check_existence(check_file, checker_source)
+        assert check_exists is True
+        check = checker_source.load_plugin(check_file)
+        check_result = check.act(parsed_arguments, remaining_arguments)
+        # check the result
+        assert check_result is not None
+        assert len(check_result) == 1
+        assert check_result[0] is expected_result
+        # check the contents of the report
+        assert report.get_result() is not None
+        assert len(report.get_result()["check"]) > 1
+        assert report.get_result()["outcome"] is expected_result
+        if expected_result:
+            assert report.get_result()["diagnostic"] == ""
+        else:
+            assert report.get_result()["diagnostic"] != ""
