3df4d303bc22660bb5f2e6fa05afb8f6377bd42b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-22 12:15:38 -0400

    test: Add parameterized test for act to test_check_MatchCommandFragment.

--- tests/checks/test_check_MatchCommandFragment.py
+++ tests/checks/test_check_MatchCommandFragment.py
@@ -1,6 +1,10 @@
 """Tests for MatchCommandFragment's input and verification of command-line arguments."""
 
 import pytest
+import os
+import sys
+
+from unittest.mock import patch
 
 from gator import arguments
 from gator import checkers
@@ -51,8 +55,28 @@ def test_optional_commandline_arguments_cannot_verify(commandline_arguments, cap
     [
         (["--command", "run_command_first", "--fragment", "fragment", "--count", "5"]),
         (["--command", "run_command_second", "--fragment", "fragment", "--count", "5"]),
-        (["--command", "run_command_first", "--fragment", "fragment", "--count", "5", "--exact"]),
-        (["--command", "run_command_second", "--fragment", "fragment", "--count", "5", "--exact"]),
+        (
+            [
+                "--command",
+                "run_command_first",
+                "--fragment",
+                "fragment",
+                "--count",
+                "5",
+                "--exact",
+            ]
+        ),
+        (
+            [
+                "--command",
+                "run_command_second",
+                "--fragment",
+                "fragment",
+                "--count",
+                "5",
+                "--exact",
+            ]
+        ),
     ],
 )
 def test_optional_commandline_arguments_can_parse_created_parser(
@@ -69,11 +93,165 @@ def test_optional_commandline_arguments_can_parse_created_parser(
     [
         (["--command", "run_command_first", "--fragment", "fragment", "--count", "5"]),
         (["--command", "run_command_second", "--fragment", "fragment", "--count", "5"]),
-        (["--command", "run_command_first", "--fragment", "fragment", "--count", "5", "--exact"]),
-        (["--command", "run_command_second", "--fragment", "fragment", "--count", "5", "--exact"]),
+        (
+            [
+                "--command",
+                "run_command_first",
+                "--fragment",
+                "fragment",
+                "--count",
+                "5",
+                "--exact",
+            ]
+        ),
+        (
+            [
+                "--command",
+                "run_command_second",
+                "--fragment",
+                "fragment",
+                "--count",
+                "5",
+                "--exact",
+            ]
+        ),
     ],
 )
 def test_required_commandline_arguments_can_parse(commandline_arguments, not_raises):
     """Ensure that correct optional command-line arguments check correctly."""
     with not_raises(SystemExit):
         _ = check_MatchCommandFragment.parse(commandline_arguments)
+
+
+@pytest.mark.parametrize(
+    "commandline_arguments, expected_result",
+    [
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                "WrongCommand",
+                "--fragment",
+                "NoFragment",
+                "--count",
+                "0",
+            ],
+            True,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                "WrongCommand",
+                "--fragment",
+                "NoFragment",
+                "--count",
+                "0",
+                "--exact",
+            ],
+            True,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                "WrongCommand",
+                "--fragment",
+                "NoFragment",
+                "--count",
+                "1000",
+            ],
+            False,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                "WrongCommand",
+                "--fragment",
+                "NoFragment",
+                "--count",
+                "1000",
+                "--exact",
+            ],
+            False,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                'echo "CorrectCommand"',
+                "--fragment",
+                "Corr",
+                "--count",
+                "1",
+            ],
+            True,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                'echo "CorrectCommand"',
+                "--fragment",
+                "Corr",
+                "--count",
+                "2",
+                "--exact",
+            ],
+            False,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                'echo "CorrectCommand"',
+                "--fragment",
+                "Corr",
+                "--count",
+                "100",
+            ],
+            False,
+        ),
+        (
+            [
+                "MatchCommandFragment",
+                "--command",
+                'echo "CorrectCommand"',
+                "--fragment",
+                "Corr",
+                "--count",
+                "100",
+                "--exact",
+            ],
+            False,
+        ),
+    ],
+)
+def test_act_produces_output(commandline_arguments, expected_result):
+    """Ensure that using the check produces output."""
+    testargs = [os.getcwd()]
+    with patch.object(sys, "argv", testargs):
+        parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
+        args_verified = arguments.verify(parsed_arguments)
+        assert args_verified is True
+        external_checker_directory = checkers.get_checker_dir(parsed_arguments)
+        checker_source = checkers.get_source([external_checker_directory])
+        check_name = checkers.get_chosen_check(parsed_arguments)
+        check_file = checkers.transform_check(check_name)
+        check_exists = checkers.verify_check_existence(check_file, checker_source)
+        assert check_exists is True
+        check = checker_source.load_plugin(check_file)
+        check_result = check.act(parsed_arguments, remaining_arguments)
+        # check the result
+        assert check_result is not None
+        assert len(check_result) == 1
+        assert check_result[0] is expected_result
+        # check the contents of the report
+        assert report.get_result() is not None
+        assert len(report.get_result()["check"]) > 1
+        assert report.get_result()["outcome"] is expected_result
+        if expected_result:
+            assert report.get_result()["diagnostic"] == ""
+        else:
+            assert report.get_result()["diagnostic"] != ""
