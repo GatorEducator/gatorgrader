80d4c1b2a1484011dcc54b8cda4b88e577707708
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-22 20:38:35 -0400

    test: Update the test_constants to look for * in Current_Directory_Glob.

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -32,7 +32,7 @@ def test_markers_constant_defined():
 def test_paths_constant_defined():
     """Check correctness for the variables in the paths constant."""
     assert constants.paths.Current_Directory == "."
-    assert constants.paths.Current_Directory_Glob == "*.*"
+    assert constants.paths.Current_Directory_Glob == "*"
     assert constants.paths.Home == "gatorgrader"
 
 
