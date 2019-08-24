7021d8428d5b495375d476c9a5ad6cd773e9e12a
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-24 17:56:03 -0400

    test: Add more complex regex test to test_check_MatchFileRegex.

--- tests/checks/test_check_MatchFileRegex.py
+++ tests/checks/test_check_MatchFileRegex.py
@@ -314,3 +314,101 @@ def test_act_produces_output(
             assert report.get_result()["diagnostic"] == ""
         else:
             assert report.get_result()["diagnostic"] != ""
+
+
+@pytest.mark.parametrize(
+    "commandline_arguments, chosen_file, containing_directory, provided_count, expected_result",
+    [
+        (
+            [
+                "MatchFileRegex",
+                "--file",
+                "file_to_find",
+                "--directory",
+                "containing_directory",
+                "--count",
+                "4",
+                "--regex",
+                "[##] [\\w\\s]* GatorGrader[?]?[\\n]",
+            ],
+            "file_to_find",
+            "containing_directory",
+            "4",
+            True,
+        )
+    ],
+)
+def test_act_produces_output_complex_regex(
+    commandline_arguments,
+    chosen_file,
+    containing_directory,
+    provided_count,
+    expected_result,
+    tmpdir,
+):
+    """Check that using the check produces output."""
+    test_contents = """
+## What Do People Think about GatorGrader?
+
+GatorGrader addresses many of the challenges that an instructor faces when
+designing automated checks for the source code or technical writing that a
+
+## Installing GatorGrader
+
+Installing GatorGrader is not necessary if you intend to use it through its
+[Gradle plugin](https://github.com/GatorEducator/gatorgradle). If you want to
+
+## Running GatorGrader
+
+Students and instructors normally use GatorGrader through its [Gradle
+plugin](https://github.com/GatorEducator/gatorgradle), specifying the requested
+
+## Testing GatorGrader
+
+### Automated Testing
+
+The developers use [Pytest](https://docs.pytest.org/en/latest/) for the testing
+of GatorGrader. Depending on your goals, there are several different..."""
+    testargs = [os.getcwd()]
+    with patch.object(sys, "argv", testargs):
+        new_file = tmpdir.mkdir(containing_directory).join(chosen_file)
+        new_file.write(test_contents)
+        assert new_file.read() == test_contents
+        assert len(tmpdir.listdir()) == 1
+        overall_directory = (
+            tmpdir.dirname + "/" + tmpdir.basename + "/" + containing_directory
+        )
+        commandline_arguments = [
+            "MatchFileRegex",
+            "--file",
+            "file_to_find",
+            "--directory",
+            overall_directory,
+            "--count",
+            provided_count,
+            "--regex",
+            "[##] [\\w\\s]* GatorGrader[?]?[\\n]",
+        ]
+        parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
+        args_verified = arguments.verify(parsed_arguments)
+        assert args_verified is True
+        external_checker_directory = checkers.get_checker_dir(parsed_arguments)
+        checker_source = checkers.get_source([external_checker_directory])
+        check_name = checkers.get_chosen_check(parsed_arguments)
+        check_file = checkers.transform_check(check_name)
+        check_exists = checkers.verify_check_existence(check_file, checker_source)
+        assert check_exists is True
+        check = checker_source.load_plugin(check_file)
+        check_result = check.act(parsed_arguments, remaining_arguments)
+        # check the result
+        assert check_result is not None
+        assert len(check_result) == 1
+        # assert check_result[0] is expected_result
+        # check the contents of the report
+        assert report.get_result() is not None
+        assert len(report.get_result()["check"]) > 1
+        assert report.get_result()["outcome"] is expected_result
+        if expected_result:
+            assert report.get_result()["diagnostic"] == ""
+        else:
+            assert report.get_result()["diagnostic"] != ""
