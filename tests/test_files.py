5ed86d1b522afc8d972fd1419d194beeaa5cba50
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-27 10:47:19 -0400

    style: Improve source code formatting in test_files.

--- tests/test_files.py
+++ tests/test_files.py
@@ -122,7 +122,9 @@ def test_garbage_glob_ext_returns_no_matching_paths(tmpdir):
     assert len(created_paths) == 0
 
 
-def test_garbage_glob_file_returns_no_matching_paths_more_garbage_markdown_names(tmpdir):
+def test_garbage_glob_file_returns_no_matching_paths_more_garbage_markdown_names(
+    tmpdir
+):
     """Ensure that creating a garbage globbed path returns no matches."""
     hello_file_one = tmpdir.join("hello1.txt")
     hello_file_one.write("content")
@@ -130,11 +132,15 @@ def test_garbage_glob_file_returns_no_matching_paths_more_garbage_markdown_names
     hello_file_two.write("content")
     assert len(tmpdir.listdir()) == 2
     created_paths = list(
-        files.create_paths(tmpdir.basename, file="%*#@@--(*.md)Hello.md", home=tmpdir.dirname)
+        files.create_paths(
+            tmpdir.basename, file="%*#@@--(*.md)Hello.md", home=tmpdir.dirname
+        )
     )
     assert len(created_paths) == 0
     created_paths = list(
-        files.create_paths(tmpdir.basename, file="Hello.md%*#@@--(*.md)", home=tmpdir.dirname)
+        files.create_paths(
+            tmpdir.basename, file="Hello.md%*#@@--(*.md)", home=tmpdir.dirname
+        )
     )
     assert len(created_paths) == 0
 
