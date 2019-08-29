c4bda7be244726e33f985492cb5721871ef08cf4
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-08-29 15:57:23 -0400

    fix: Call invoke_all_total_word_count_checks in check_CountFileWords.

--- gator/checks/check_CountFileWords.py
+++ gator/checks/check_CountFileWords.py
@@ -3,6 +3,8 @@
 import argparse
 
 from gator import checkers
+from gator import constants
+from gator import fragments
 from gator import invoke
 
 
@@ -86,4 +88,13 @@ def act(main_parsed_arguments, check_remaining_arguments):
     file = check_parsed_arguments.file
     directory = check_parsed_arguments.directory
     exact = check_parsed_arguments.exact
-    return [invoke.invoke_all_total_word_count_checks(file, directory, count, exact)]
+    return [
+        invoke.invoke_all_total_word_count_checks(
+            file,
+            directory,
+            count,
+            fragments.count_total_words,
+            constants.words.Total,
+            exact,
+        )
+    ]
