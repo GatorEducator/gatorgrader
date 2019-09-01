6a495b2bd407a90b25ef0ef267da669d250e85b1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-09-01 17:55:51 -0400

    style: Format the source code in test_invoke.

--- tests/test_invoke.py
+++ tests/test_invoke.py
@@ -170,7 +170,7 @@ def test_file_exists_in_directory_check_words_exact(reset_results_dictionary, tm
         4,
         fragments.count_minimum_words,
         constants.words.Minimum,
-        True
+        True,
     )
     details = report.get_result()
     assert details is not None
@@ -181,7 +181,7 @@ def test_file_exists_in_directory_check_words_exact(reset_results_dictionary, tm
         200,
         fragments.count_minimum_words,
         constants.words.Minimum,
-        True
+        True,
     )
     details = report.get_result()
     assert details is not None
