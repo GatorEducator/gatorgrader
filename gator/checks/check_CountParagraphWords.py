4c10d128aa543900009ac44fb82a0b4e09465b15
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-09-05 10:57:26 -0400

    fix: Delete a not-needed metavar in check_CountParagraphWords.

--- gator/checks/check_CountParagraphWords.py
+++ gator/checks/check_CountParagraphWords.py
@@ -43,7 +43,6 @@ def get_parser():
     required_group.add_argument(
         "--count",
         type=int,
-        metavar="COUNT",
         help="how many words should exist in every paragraph",
         required=True,
     )
