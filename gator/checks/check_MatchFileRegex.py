2788788e2e593664ffaa7d8ca6271fea8c63460e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-09-05 11:02:39 -0400

    fix: Improve argument descr and metavars in check_MatchFileRegex.

--- gator/checks/check_MatchFileRegex.py
+++ gator/checks/check_MatchFileRegex.py
@@ -49,11 +49,7 @@ def get_parser():
     # COUNT: the number of lines of output
     # REQUIRED? Yes
     required_group.add_argument(
-        "--count",
-        type=int,
-        metavar="COUNT",
-        help="how many of an entity should exist",
-        required=True,
+        "--count", type=int, help="how many regex matches should exist", required=True
     )
 
     # }}}
