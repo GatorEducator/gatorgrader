a19a44e69b0b63c0809e114acbcea918544eb336
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-09-05 11:04:25 -0400

    fix: Delete a not-needed metavar in check_CountFileParagraphs.

--- gator/checks/check_CountFileParagraphs.py
+++ gator/checks/check_CountFileParagraphs.py
@@ -39,11 +39,7 @@ def get_parser():
     # COUNT: the number of lines of output
     # REQUIRED? Yes
     required_group.add_argument(
-        "--count",
-        type=int,
-        metavar="COUNT",
-        help="how many paragraphs should exist",
-        required=True,
+        "--count", type=int, help="how many paragraphs should exist", required=True
     )
 
     # }}}
