d9c8e680ca8f5755f98b4501b975327ed93204e3
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2019-09-05 11:03:46 -0400

    fix: Improve arguments and metavars in check_MatchFileFragment.

--- gator/checks/check_MatchFileFragment.py
+++ gator/checks/check_MatchFileFragment.py
@@ -40,17 +40,17 @@ def get_parser():
     # FRAGMENT: the fragment that should appear in the command's output
     # REQUIRED? Yes
     required_group.add_argument(
-        "--fragment", type=str, help="fragment that exists in the file", required=True
+        "--fragment",
+        type=str,
+        metavar="FRAG",
+        help="fragment that exists in the file",
+        required=True,
     )
 
     # COUNT: the number of lines of output
     # REQUIRED? Yes
     required_group.add_argument(
-        "--count",
-        type=int,
-        metavar="COUNT",
-        help="how many of an entity should exist",
-        required=True,
+        "--count", type=int, help="how many of a fragment should exist", required=True
     )
 
     # }}}
