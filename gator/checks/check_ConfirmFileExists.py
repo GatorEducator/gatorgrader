00f1dc1d6f9c48b78772b1d7ba0bb2e2bba8681a
Megan Corletti <corlettim@allegheny.edu>
2020-02-04 22:03:33 -0500

    add optional reach argument to confirm file exists

--- gator/checks/check_ConfirmFileExists.py
+++ gator/checks/check_ConfirmFileExists.py
@@ -41,6 +41,14 @@ def get_parser():
     # Optional Named Checker Arguments {{{
 
     # None required for this checker
+    # REACH: allows for a students to have a reach goal in their lab/practical
+    # REQUIRED? No
+    optional_group.add_argument(
+        "--reach",
+        help="creates a higher goal for students to potentially reach",
+        default = False,
+        action = "store_true",
+    )
 
     # }}}
     return parser
