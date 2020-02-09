f06642668dc4a1f1b0f72dd8ea20922af36f8432
Megan Corletti <corlettim@allegheny.edu>
2020-02-09 16:17:38 -0500

    fix assertion errors for new lines to equal 3 instead of 2

--- tests/checks/test_check_CountFileWords.py
+++ tests/checks/test_check_CountFileWords.py
@@ -23,7 +23,7 @@ def test_no_arguments_incorrect_system_exit(capsys):
     # standard error has two lines from pytest
     assert "usage:" in captured.err
     counted_newlines = captured.err.count("\n")
-    assert counted_newlines == 2
+    assert counted_newlines == 3
 
 
 @pytest.mark.parametrize(
@@ -49,7 +49,7 @@ def test_required_commandline_arguments_cannot_parse(commandline_arguments, caps
     # standard error has two lines from pytest
     assert "usage:" in captured.err
     counted_newlines = captured.err.count("\n")
-    assert counted_newlines == 2
+    assert counted_newlines == 3
 
 
 @pytest.mark.parametrize(
