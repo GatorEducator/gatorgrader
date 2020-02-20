2ae541e3c5b2dab236fd24a105ec778feaa5f16e
Megan Corletti <corlettim@allegheny.edu>
2020-02-20 11:44:18 -0500

    add reach=False for count commits in invoke

--- gator/checks/check_CountFileLines.py
+++ gator/checks/check_CountFileLines.py
@@ -91,9 +91,9 @@ def act(main_parsed_arguments, check_remaining_arguments):
     file = check_parsed_arguments.file
     directory = check_parsed_arguments.directory
     exact = check_parsed_arguments.exact
-    # reach = check_parsed_arguments.reach
+    reach = check_parsed_arguments.reach
     return [
         invoke.invoke_all_count_checks(
-            count, file, directory, constants.markers.Nothing, exact
+            count, file, directory, constants.markers.Nothing, exact, reach
         )
     ]
