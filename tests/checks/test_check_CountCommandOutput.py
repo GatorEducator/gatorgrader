b2a7fd861d93d80ccfa0e1253e082b63a2eea6f2
Megan Corletti <corlettim@allegheny.edu>
2020-02-27 19:18:39 -0500

    change assertion lines for command output

--- tests/checks/test_check_CountCommandOutput.py
+++ tests/checks/test_check_CountCommandOutput.py
@@ -22,7 +22,7 @@ def test_no_arguments_incorrect_system_exit(capsys):
     # standard error has two lines from pytest
     assert "usage:" in captured.err
     counted_newlines = captured.err.count("\n")
-    assert counted_newlines == 3
+    assert counted_newlines == 2
 
 
 @pytest.mark.parametrize(
@@ -46,7 +46,7 @@ def test_required_commandline_arguments_cannot_parse(commandline_arguments, caps
     # standard error has two lines from pytest
     assert "usage:" in captured.err
     counted_newlines = captured.err.count("\n")
-    assert counted_newlines == 3
+    assert counted_newlines == 2
 
 
 @pytest.mark.parametrize(
