994304d044473bb1de7a12b0c22f5f023913f669
Megan Corletti <corlettim@allegheny.edu>
2020-03-10 12:02:07 -0400

    add reach case to count filelines

--- tests/checks/test_check_CountFileLines.py
+++ tests/checks/test_check_CountFileLines.py
@@ -36,6 +36,17 @@ def test_no_arguments_incorrect_system_exit(capsys):
         (["--file", "filename", "--directoryWRONG", "directory"]),
         (["--file", "filename", "--directory", "directory", "--count"]),
         (["--file", "filename", "--directory", "directory", "--countWRONG", "5"]),
+        (
+            [
+                "--file",
+                "filename",
+                "--directory",
+                "directory",
+                "--countWRONG",
+                "5",
+                "--reach",
+            ]
+        ),
     ],
 )
 def test_required_commandline_arguments_cannot_parse(commandline_arguments, capsys):
@@ -201,6 +212,7 @@ def test_act_produces_output(
             overall_directory,
             "--count",
             provided_count,
+            "--reach",
         ]
         parsed_arguments, remaining_arguments = arguments.parse(commandline_arguments)
         args_verified = arguments.verify(parsed_arguments)
