fd915155cee0b6b31ec4e4f6da05076d17961221
Simon Jones <github@simonjjones.com>
2020-10-15 00:15:50 -0400

    helper functions for integration testing

--- scripts/ensure.py
+++ scripts/ensure.py
@@ -1,10 +1,73 @@
-import json
+"""integration testing to ensure grading consistency"""
+
 import requests
+from subprocess import run
+import os
+from shutil import rmtree
+
+# FIXME: how do they want me to handle exceptions on the upper level?
+# FIXME: integrate into CI
+# FIXME: test with gradle (I don't have it on my workstation yet)
+# FIXME: windows/mac support
+
+GITHUB_API_URI = "https://api.github.com/orgs/GatorEducator/repos"
+REPOS_DIR = "repos"
+ORIGINAL_DIR = ""
 
 def fetch_repos():
-    response = requests.get("https://api.github.com/orgs/GatorEducator/repos", headers={"Accept": "application/vnd.github.v3+json"})
+    """fetches starter/solution repositories from GatorEducator; returns two tuple arrays"""
+    global GITHUB_API_URI
+    response = requests.get(GITHUB_API_URI, headers={"Accept": "application/vnd.github.v3+json"})
     repos = response.json()
-    # ("REPOSITORY_NAME", "CLONE_URL")
-    repodata = [(repo["name"], repo["clone_url"]) for repo in repos]
+    repodata = {"starters": [], "solutions": []}
+    for repo in repos:
+        if "assignment-start" in repo["name"]:
+            repodata["starters"].append((repo["name"], repo["clone_url"]))
+        elif "assignment-solution" in repo["name"]:
+            repodata["solutions"].append((repo["name"], repo["clone_url"]))
     return repodata
+
+def grade_repo(repo):
+    """clones and grades a repository; returns True if it passes, False if it fails"""
+    proc1 = subprocess.run(["git clone", repo[1]], check=True)
+    os.chdir(repo[0])
+    proc2 = subprocess.run(["gradle grade"])
+    return True if proc2.returncode == 0 else False
+
+# should fail
+def grade_starter(repo):
+    if not grade_repo(repo):
+        return None
+    else:
+        raise Exception(f"{repo[0]} passed when it should have failed.")
+
+# should pass
+def grade_solution(repo):
+    if grade_repo(repo):
+        return None
+    else:
+        raise Exception(f"{repo[0]} failed when it should have passed.")
+
+def cleanup():
+    global REPOS_DIR
+    if os.path.exists(REPOS_DIR):
+        shutil.rmtree(REPOS_DIR)
+
+def setup():
+    global ORIGINAL_DIR, REPOS_DIR
+    ORIGINAL_DIR = os.getcwd()
+    os.mkdir(REPOS_DIR)
+    os.chdir(REPOS_DIR)
+
+def ensure():
+    try:
+        repos = fetch_repos()
+        for starter in repos["starters"]:
+            grade_starter(starter)
+        for solution in repos["solutions"]:
+            grade_solution(solution)
+    except Exception as e:
+        print(e)
+    finally:
+        cleanup()
 
