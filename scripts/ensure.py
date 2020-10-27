b34634bfa5c68a497cd39198824c89688f079831
Simon Jones <github@simonjjones.com>
2020-10-27 01:56:13 -0400

    added more documentation & fixed bug with directory scaffolding

--- scripts/ensure.py
+++ scripts/ensure.py
@@ -1,4 +1,14 @@
-"""integration testing to ensure grading consistency"""
+"""integration testing to ensure grading consistency
+
+To activate this script, ensure() must be called.
+
+* __setup() begins by creating a temporary folder in which the repositories will be tested.
+* __fetch_repos() then fetches names and urls of repositories to test, separating the starters and the solutions.
+* __grade_starter() and __grade_solution() both interface __grade_repo(), which uses `gradle` to grade each repository (True if pass, False if fail).
+* Both __grade_starter() and __grade_solution() raise an exception if the output is unexpected.
+* Ultimately, __cleanup() removes the temporary directory and all of its contents, leaving no trace of the testing.
+"""
+
 
 import requests
 from subprocess import run
@@ -13,7 +23,7 @@ REPOS_DIR = "repos"
 ORIGINAL_DIR = ""
 
 
-def fetch_repos():
+def __fetch_repos():
     """fetches starter/solution repositories from GatorEducator; returns two tuple arrays"""
     global GITHUB_API_URI
     response = requests.get(GITHUB_API_URI, headers={"Accept": "application/vnd.github.v3+json"})
@@ -27,61 +37,68 @@ def fetch_repos():
     return repodata
 
 
-def grade_repo(repo):
+def __grade_repo(repo):
     """clones and grades a repository; returns True if it passes, False if it fails"""
-    proc1 = subprocess.run(["git clone", repo[1]], check=True)
+    proc1 = run(["git", "clone", repo[1]], check=True)
     os.chdir(repo[0])
-    proc2 = subprocess.run(["gradle grade"])
+    proc2 = run(["gradle", "grade"])
+    os.chdir(REPOS_DIR)
     return True if proc2.returncode == 0 else False
 
 
 # should fail
-def grade_starter(repo):
-    """wrapper for starter repository, see grade_repo()"""
-    if not grade_repo(repo):
-        return None
+def __grade_starter(repo):
+    """wrapper for starter repository, see __grade_repo()"""
+    if not __grade_repo(repo):
+        return None # good
     else:
         raise Exception(f"{repo[0]} passed when it should have failed.")
 
 
 # should pass
-def grade_solution(repo):
-    """wrapper for solution repository, see grade_repo()"""
-    if grade_repo(repo):
-        return None
+def __grade_solution(repo):
+    """wrapper for solution repository, see __grade_repo()"""
+    if __grade_repo(repo):
+        return None # good
     else:
         raise Exception(f"{repo[0]} failed when it should have passed.")
 
 
-def cleanup():
+def __cleanup():
     """removes temporary directory used in testing"""
     global REPOS_DIR
     if os.path.exists(REPOS_DIR):
-        shutil.rmtree(REPOS_DIR)
+        rmtree(REPOS_DIR)
 
 
-def setup():
+def __setup():
     """gets current working directory and sets up temporary directory used in testing"""
     global ORIGINAL_DIR, REPOS_DIR
     ORIGINAL_DIR = os.getcwd()
-    os.mkdir(REPOS_DIR)
+    if not os.path.exists(REPOS_DIR):
+        os.mkdir(REPOS_DIR)
     os.chdir(REPOS_DIR)
+    # get absolute path
+    REPOS_DIR = os.getcwd()
 
 
 def ensure():
     """module wrapper; initiates testing"""
     try:
-        repos = fetch_repos()
+        __setup()
+        repos = __fetch_repos()
         for starter in repos["starters"]:
-            grade_starter(starter)
+            __grade_starter(starter)
         for solution in repos["solutions"]:
-            grade_solution(solution)
+            __grade_solution(solution)
     except Exception as e:
         print(e)
     finally:
-        cleanup()
+        __cleanup()
+
 
 # FIXME: how do they want me to handle exceptions on the upper level?
 # FIXME: integrate into CI
 # FIXME: test with gradle (I don't have it on my workstation yet)
 # FIXME: windows/mac support
+# FIXME: async
