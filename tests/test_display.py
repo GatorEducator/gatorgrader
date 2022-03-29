ecc3dcc50258481670dc9de891db4037dc43d2c5
Saejin Heinert <saejinmh@gmail.com>
2022-03-29 11:33:51 -0400

    Add tests for new and rewritten display functions

--- tests/test_display.py
+++ tests/test_display.py
@@ -1,6 +1,7 @@
 """Tests for the display module.."""
 
 from gator import display
+from gator.exceptions import InvalidCheckArgumentsError, InvalidCheckError
 
 
 def test_display_welcome_produce_output_line_count(capsys):
@@ -13,13 +14,51 @@ def test_display_welcome_produce_output_line_count(capsys):
     assert captured.err == ""
 
 
-def test_display_invalid_produce_output_line_count(capsys):
-    """Check that the invalid arguments function produces output lines."""
+def test_display_invalid_system_arguments(capsys):
+    """Check that the invalid system arguments function produces output lines."""
     display.incorrect_system_arguments_message()
     captured = capsys.readouterr()
     counted_newlines = captured.out.count("\n")
     assert "Incorrect" in captured.out
     assert counted_newlines == 2
+    assert captured.err == ""
+
+
+def test_display_invalid_system_arguments_with_error(capsys):
+    """Check that the invalid system arguments function produces output lines."""
+    display.incorrect_system_arguments_message(InvalidCheckError([], "TestCheck"))
+    captured = capsys.readouterr()
+    counted_newlines = captured.out.count("\n")
+    assert "Incorrect command-line" in captured.out
+    assert "TestCheck" in captured.out
+    assert counted_newlines == 3
+    assert captured.err == ""
+
+
+def test_display_invalid_check_arguments(capsys):
+    """Check that the invalid check arguments function produces output lines."""
+    display.incorrect_check_arguments_message()
+    captured = capsys.readouterr()
+    counted_newlines = captured.out.count("\n")
+    assert "Incorrect check" in captured.out
+    assert counted_newlines == 2
+    assert captured.err == ""
+
+
+def test_display_invalid_check_arguments_with_error(capsys):
+    """Check that the invalid check arguments function produces output lines."""
+    display.incorrect_check_arguments_message(
+        InvalidCheckArgumentsError(
+            [], "usage: TestCheck", "TestCheck: some error happened"
+        )
+    )
+    captured = capsys.readouterr()
+    counted_newlines = captured.out.count("\n")
+    assert "Incorrect check" in captured.out
+    assert "usage:" in captured.out
+    assert captured.out.count("TestCheck") == 2
+    assert "some error" in captured.out
+    assert counted_newlines == 5
     assert captured.err == ""
 
 
