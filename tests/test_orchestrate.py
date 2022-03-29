95173c5df67170f57e0421ee10c351e97b20ab6a
Saejin Heinert <saejinmh@gmail.com>
2022-03-29 10:11:24 -0400

    Remove unneeded orchestrate tests

--- tests/test_orchestrate.py
+++ tests/test_orchestrate.py
@@ -1,48 +1,10 @@
 """Test cases for the orchestrate module."""
 
 import pytest
-import os
-import sys
 
 from unittest.mock import patch
 
-from gator import constants
 from gator import orchestrate
-from gator import report
-
-
-def test_perform_actions_no_parameters_welcome(capsys):
-    """Check to see if perform can invoke welcome action with no parameters."""
-    actions = []
-    actions.append([orchestrate.DISPLAY, "welcome_message", []])
-    orchestrate.perform_actions(actions)
-    captured = capsys.readouterr()
-    counted_newlines = captured.out.count("\n")
-    assert "GatorGrader" in captured.out
-    assert counted_newlines == 4
-    assert captured.err == ""
-
-
-def test_perform_actions_no_parameters_incorrect(capsys):
-    """Check to see if perform can invoke incorrect action with no parameters."""
-    actions = []
-    actions.append([orchestrate.DISPLAY, "incorrect_message", []])
-    orchestrate.perform_actions(actions)
-    captured = capsys.readouterr()
-    counted_newlines = captured.out.count("\n")
-    assert "Incorrect" in captured.out
-    assert counted_newlines == 2
-    assert captured.err == ""
-
-
-def test_perform_actions_single_parameter_exit():
-    """Check to see if perform can invoke exit actions with a parameter."""
-    actions = []
-    with pytest.raises(SystemExit) as pytest_wrapped_e:
-        actions.append([orchestrate.RUN, "run_exit", [constants.arguments.Incorrect]])
-        orchestrate.perform_actions(actions)
-    assert pytest_wrapped_e.type == SystemExit
-    assert pytest_wrapped_e.value.code == 2
 
 
 @pytest.mark.parametrize(
