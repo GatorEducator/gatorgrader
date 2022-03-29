68da835c3cb30da5c580a6cc8c363008ede5f3b3
Saejin Heinert <saejinmh@gmail.com>
2022-03-29 07:45:56 -0400

    Expose orchestrate's main_api() as gator.grader for consumers of the gator package

--- gator/__init__.py
+++ gator/__init__.py
@@ -1,1 +1,6 @@
-"""Empty file required by pylint."""
+"""Gator package initilization."""
+
+# expose main_api entry point as "grader" function
+from gator.orchestrate import main_api as grader  # noqa: F401
+from gator.orchestrate import InvalidSystemArgumentsError  # noqa: F401
+from gator.orchestrate import InvalidCheckError  # noqa: F401
