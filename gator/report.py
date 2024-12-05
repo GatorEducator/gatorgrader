8efcc71a60ca178e6ebdd6bf9c9de01187648fc9
Rebekah Rudd <rudd01@allegheny.edu>
2024-12-05 17:17:58 -0500

    added another two linting updates

--- gator/report.py
+++ gator/report.py
@@ -42,7 +42,7 @@ def decompose_result(result_dictionary):
 def reset():
     """Reset the global result dictionary."""
     # pylint: disable=global-statement
-    global result
+    # global result
     result = None
 
 
